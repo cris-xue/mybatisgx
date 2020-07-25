f63628ebfb2242d60ebc002841844c1802cdbd93
cris <ccxuef@isoftstone.com>
2020-07-25 21:31:46 +0800

    1、增加动态判断参数是否为一个，以及其它类型 2、优化不解析方法

--- mybatisx-core/src/main/java/com/lc/mybatisx/handler/AbstractMapperHandler.java
+++ mybatisx-core/src/main/java/com/lc/mybatisx/handler/AbstractMapperHandler.java
@@ -7,8 +7,9 @@ import org.slf4j.LoggerFactory;
 import sun.reflect.generics.reflectiveObjects.ParameterizedTypeImpl;
 
 import javax.persistence.Table;
-import java.lang.reflect.Method;
-import java.lang.reflect.Type;
+import java.lang.reflect.*;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author ：薛承城
@@ -20,13 +21,12 @@ public abstract class AbstractMapperHandler {
     private static final Logger log = LoggerFactory.getLogger(AbstractMapperHandler.class);
 
     protected Class<?> getDaoInterface(String namespace) {
-        Class<?> daoInterface = null;
         try {
-            daoInterface = Class.forName(namespace);
+            return Class.forName(namespace);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-        return daoInterface;
+        return null;
     }
 
     protected Type[] getDaoInterfaceParams(Class<?> daoInterface, Class<?> CURDInterface) {
@@ -47,18 +47,76 @@ public abstract class AbstractMapperHandler {
 
     protected SqlWrapper buildSqlWrapper(String namespace, Method method, Type[] daoInterfaceParams) {
         String methodName = method.getName();
-        Class<?> entityClass = (Class<?>) daoInterfaceParams[0];
-        String tableName = entityClass.getAnnotation(Table.class).name();
         Class<?> idClass = (Class<?>) daoInterfaceParams[1];
+        Class<?> entityClass = (Class<?>) daoInterfaceParams[0];
+        String tableName = this.getTableName(entityClass);
 
         SqlWrapper sqlWrapper = instanceSqlWrapper();
         sqlWrapper.setNamespace(namespace);
         sqlWrapper.setMethodName(methodName);
-        sqlWrapper.setParameterType(entityClass.getName());
+        String parameterType = this.getParameterType(method, entityClass);
+        sqlWrapper.setParameterType(parameterType);
         sqlWrapper.setTableName(tableName);
-        sqlWrapper.setResultType(entityClass.getName());
+        String resultType = this.getResultType(method, entityClass);
+        sqlWrapper.setResultType(resultType);
 
         return sqlWrapper;
+    }
+
+    /**
+     * 获取表名
+     *
+     * @param entityClass
+     * @return
+     */
+    private String getTableName(Class<?> entityClass) {
+        Table table = entityClass.getAnnotation(Table.class);
+        return table != null ? table.name() : entityClass.getSimpleName();
+    }
+
+    private String getParameterType(Method method, Class<?> entityClass) {
+        Parameter[] parameters = method.getParameters();
+        if (parameters.length == 1) {
+            TypeVariable typeVariable = (TypeVariable) parameters[0].getParameterizedType();
+            String parameterName = typeVariable.getName();
+            if ("ENTITY".equals(parameterName)) {
+                return entityClass.getName();
+            } else {
+
+            }
+        }
+        return null;
+    }
+
+    private String getResultType(Method method, Class<?> entityClass) {
+        Type type = method.getGenericReturnType();
+        if (type instanceof Class<?>) {
+            Class<?> resultType = (Class<?>) type;
+            return resultType.getName();
+        } else if (type instanceof ParameterizedType) {
+            ParameterizedType parameterizedType = (ParameterizedType) method.getGenericReturnType();
+            Type rawType = parameterizedType.getRawType();
+            Type[] actualTypeArguments = parameterizedType.getActualTypeArguments();
+            if (rawType instanceof Map<?, ?>) {
+                Class<?> resultType = (Class<?>) rawType;
+                return resultType.getName();
+            } else if (rawType instanceof Class<?> && rawType == List.class) {
+                TypeVariable typeVariable = (TypeVariable) actualTypeArguments[0];
+                if ("ENTITY".equals(typeVariable.getName())) {
+                    return entityClass.getName();
+                }
+                return typeVariable.getName();
+            } else {
+                // rawType
+                return null;
+            }
+        } else if (type instanceof TypeVariable<?>) {
+            TypeVariable<?> typeVariable = (TypeVariable<?>) type;
+            if ("ENTITY".equals(typeVariable.getName())) {
+                return entityClass.getName();
+            }
+        }
+        return null;
     }
 
     abstract protected SqlWrapper instanceSqlWrapper();
--- mybatisx-core/src/main/java/com/lc/mybatisx/handler/ConditionMapperHandler.java
+++ mybatisx-core/src/main/java/com/lc/mybatisx/handler/ConditionMapperHandler.java
@@ -4,7 +4,9 @@ import com.lc.mybatisx.wrapper.WhereWrapper;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author ：薛承城
@@ -13,11 +15,19 @@ import java.util.List;
  */
 public class ConditionMapperHandler {
 
+    private static Map<String, Boolean> unParseMap = new HashMap<>();
+
+    static {
+        // 会内存泄漏
+        unParseMap.put("findAll", false);
+        unParseMap.put("find", false);
+    }
+
     public List<WhereWrapper> buildWhereWrapper(Method method) {
         String methodName = method.getName();
 
         List<WhereWrapper> whereWrapperList = new ArrayList<>();
-        if (!"findAll".equals(methodName)) {
+        if (unParseMap.getOrDefault(methodName, true)) {
             String[] methodNames = methodName.split("By");
             String[] wheres = methodNames[1].split("And|Or");
             for (int i = 0; i < wheres.length; i++) {
@@ -28,7 +38,6 @@ public class ConditionMapperHandler {
                 whereWrapperList.add(whereWrapper);
             }
         }
-
         return whereWrapperList;
     }
 

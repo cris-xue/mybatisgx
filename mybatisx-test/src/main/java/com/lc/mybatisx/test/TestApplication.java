81ebe334966ce81037cf93bedad1ba7f44086de1
cris <xcc137396549@foxmail.com>
2020-07-28 22:56:25 +0800

    1、初始化mybatisx-test

---
+++ mybatisx-test/src/main/java/com/lc/mybatisx/test/TestApplication.java
@@ -0,0 +1,13 @@
+package com.lc.mybatisx.test;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Enable
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ mybatisx-test/src/main/java/com/lc/mybatisx/test/dao/TestDao.java
@@ -0,0 +1,8 @@
+package com.lc.mybatisx.test.dao;
+
+import com.lc.mybatisx.dao.SimpleDao;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TestDao extends SimpleDao {
+}

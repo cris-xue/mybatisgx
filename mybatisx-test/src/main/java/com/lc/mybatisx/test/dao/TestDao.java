ddd7e5ed3aec2ce1a19c7dfb1e0eacc39a75e5b3
cris <xcc137396549@foxmail.com>
2020-07-29 01:23:27 +0800

    1、增加多条件复杂方法

--- mybatisx-test/src/main/java/com/lc/mybatisx/test/dao/TestDao.java
+++ mybatisx-test/src/main/java/com/lc/mybatisx/test/dao/TestDao.java
@@ -1,9 +1,16 @@
 package com.lc.mybatisx.test.dao;
 
+import com.lc.mybatisx.annotation.MapperMethod;
+import com.lc.mybatisx.annotation.MethodType;
 import com.lc.mybatisx.dao.SimpleDao;
 import com.lc.mybatisx.test.model.entity.Test;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TestDao extends SimpleDao<Test, Long> {
+
+    @MapperMethod(type = MethodType.UPDATE)
+    int updateByIdAndNameOrUsername(@Param("id") Long id, @Param("name") String name, @Param("username") String username);
+
 }

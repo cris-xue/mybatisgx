f6df16ab59d6d88f22d1cc284e370576bb9131b0
cris <ccxuef@isoftstone.com>
2020-08-06 18:19:10 +0800

    1、修改配置和更改schema

--- mybatisx-test/src/main/resources/db/schema.sql
+++ mybatisx-test/src/main/resources/db/schema.sql
@@ -1,15 +1,17 @@
+create database if not exists mybatisx;
+
 create table if not exists mybatisx
 (
-    id            bigint(20) not null auto_increment,
-    input_user_id bigint(20) not null comment '输入用户id',
-    input_time    datetime   not null comment '输入时间',
-    name          char(16)   null comment '',
-    age           int(3)     null comment '',
-    user_name     char(32)   not null comment '',
-    password      char(128)  not null comment '',
-    pay_status    int(2)     not null default 1 comment '',
-    version       int(10)    not null default 1 comment '',
+	id            bigint(20) not null auto_increment,
+	input_user_id bigint(20) not null comment '输入用户id',
+	input_time    datetime   not null comment '输入时间',
+	name          char(16)   null comment '',
+	age           int(3)     null comment '',
+	user_name     char(32)   not null comment '',
+	password      char(128)  not null comment '',
+	pay_status    int(2)     not null default 1 comment '',
+	version       int(10)    not null default 1 comment '',
 
-    primary key (id)
+	primary key (id)
 ) engine = InnoDB
   default charset = utf8mb4;

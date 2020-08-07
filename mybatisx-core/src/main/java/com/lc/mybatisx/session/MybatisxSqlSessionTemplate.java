6d1982e0e1b01fa1ea15b72ddf6e54e729d2218a
cris <ccxuef@isoftstone.com>
2020-08-07 15:45:57 +0800

    1、删除无用代码

--- mybatisx-core/src/main/java/com/lc/mybatisx/session/MybatisxSqlSessionTemplate.java
+++ mybatisx-core/src/main/java/com/lc/mybatisx/session/MybatisxSqlSessionTemplate.java
@@ -1,16 +1,9 @@
 package com.lc.mybatisx.session;
 
-import org.apache.ibatis.mapping.MappedStatement;
-import org.apache.ibatis.mapping.SqlSource;
-import org.apache.ibatis.scripting.xmltags.DynamicSqlSource;
-import org.apache.ibatis.scripting.xmltags.SqlNode;
-import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.ExecutorType;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.dao.support.PersistenceExceptionTranslator;
-
-import java.lang.reflect.Field;
 
 public class MybatisxSqlSessionTemplate extends SqlSessionTemplate {
 
@@ -45,46 +38,7 @@ public class MybatisxSqlSessionTemplate extends SqlSessionTemplate {
 
     @Override
     public int delete(String statement, Object parameter) {
-        Configuration configuration = getConfiguration();
-        MappedStatement mappedStatement = configuration.getMappedStatement(statement);
-        SqlSource sqlSource = mappedStatement.getSqlSource();
-        try {
-            Field field = DynamicSqlSource.class.getDeclaredField("rootSqlNode");
-            SqlNode sqlNode = (SqlNode) field.get(sqlSource);
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            e.printStackTrace();
-        }
         return super.delete(statement, parameter);
-    }
-
-    public Object convertArgsToSqlCommandParam(Object[] args) {
-        return getNamedParams(args);
-    }
-
-    public Object getNamedParams(Object[] args) {
-        System.out.println(args);
-
-        return args;
-        /*final int paramCount = names.size();
-        if (args == null || paramCount == 0) {
-            return null;
-        } else if (!hasParamAnnotation && paramCount == 1) {
-            return args[names.firstKey()];
-        } else {
-            final Map<String, Object> param = new MapperMethod.ParamMap<>();
-            int i = 0;
-            for (Map.Entry<Integer, String> entry : names.entrySet()) {
-                param.put(entry.getValue(), args[entry.getKey()]);
-                // add generic param names (param1, param2, ...)
-                final String genericParamName = GENERIC_NAME_PREFIX + (i + 1);
-                // ensure not to overwrite parameter named with @Param
-                if (!names.containsValue(genericParamName)) {
-                    param.put(genericParamName, args[entry.getKey()]);
-                }
-                i++;
-            }
-            return param;
-        }*/
     }
 
 }

c45a63cf28ff5f5350bc5d70f3250f0514b25407
cris <ccxuef@isoftstone.com>
2020-09-10 16:38:17 +0800

    1、抽象方法

--- mybatisx-core/src/main/java/com/lc/mybatisx/handler/CURDMapper.java
+++ mybatisx-core/src/main/java/com/lc/mybatisx/handler/CURDMapper.java
@@ -1,10 +1,23 @@
 package com.lc.mybatisx.handler;
 
+import com.lc.mybatisx.dao.Dao;
+import com.lc.mybatisx.dao.SimpleDao;
+import com.lc.mybatisx.wrapper.WhereWrapper;
+import com.lc.mybatisx.wrapper.where.Keyword;
 import org.apache.ibatis.builder.MapperBuilderAssistant;
 import org.apache.ibatis.parsing.XNode;
+import org.apache.ibatis.session.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.TypeUtils;
+import sun.reflect.generics.reflectiveObjects.ParameterizedTypeImpl;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author ：薛承城
@@ -13,7 +26,26 @@ import java.util.List;
  */
 public class CURDMapper {
 
+    private static final Logger logger = LoggerFactory.getLogger(AbstractMapperHandler.class);
+
     public static List<XNode> getNodeList(MapperBuilderAssistant builderAssistant, String namespace) {
+        Class<?> daoInterface = getDaoInterface(namespace);
+        Type[] daoInterfaceParams = getDaoInterfaceParams(daoInterface);
+        Method[] methods = daoInterface.getMethods();
+        List<Method> methodList = new ArrayList<>();
+        for (int i = 0; i < methods.length; i++) {
+            Method method = methods[i];
+            Configuration configuration = builderAssistant.getConfiguration();
+            if (configuration.hasStatement(namespace + "." + method.getName())) {
+                continue;
+            }
+
+            List<String> methodKeywordList = parseMethodKeyword(method.getName());
+            WhereWrapper whereWrapper = Keyword.buildWhereWrapper(methodKeywordList);
+
+            methodList.add(method);
+        }
+
         InsertMapperHandler insertMapperHandler = new InsertMapperHandler(builderAssistant, namespace);
         List<XNode> insertList = insertMapperHandler.readTemplate();
 
@@ -32,6 +64,47 @@ public class CURDMapper {
         curdList.addAll(updateList);
         curdList.addAll(deleteList);
         return curdList;
+    }
+
+    protected static Class<?> getDaoInterface(String namespace) {
+        try {
+            return Class.forName(namespace);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    protected static Type[] getDaoInterfaceParams(Class<?> daoInterface) {
+        Type[] daoSuperInterfaces = daoInterface.getGenericInterfaces();
+        for (int i = 0; i < daoSuperInterfaces.length; i++) {
+            Type daoSuperInterfaceType = daoSuperInterfaces[i];
+            ParameterizedTypeImpl daoSuperInterfaceClass = (ParameterizedTypeImpl) daoSuperInterfaceType;
+            Type[] daoInterfaceParams = daoSuperInterfaceClass.getActualTypeArguments();
+            if (TypeUtils.isAssignable(Dao.class, daoInterface)) {
+                return daoInterfaceParams;
+            }
+        }
+
+        logger.info("{} un extend {}", SimpleDao.class.getName());
+        return null;
+    }
+
+    public static List<String> parseMethodKeyword(String methodName) {
+        // methodName = "findTop10ByIdAndNameIsOrAgeLessThanAndAgeLessThan";
+        // updateByIdSelect
+        // findById、findByIs、findByNameIsAndAgeIs
+        // 创建 Pattern 对象
+        String regex = "[a-z]+|By|And|Or|GroupBy|OrderBy|[A-Z][a-z]+|[0-9]+";
+        Pattern pattern = Pattern.compile(regex);
+        // 创建 matcher 对象
+        List<String> methodKeywordList = new ArrayList<>();
+        Matcher matcher = pattern.matcher(methodName);
+        while (matcher.find()) {
+            methodKeywordList.add(matcher.group());
+        }
+
+        return methodKeywordList;
     }
 
 }

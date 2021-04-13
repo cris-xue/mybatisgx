1551ed596c2caae04189dcd04a576d4e4bab862e
cris <ccxuef@isoftstone.com>
2021-04-13 11:52:33 +0800

    1、增加xml驱动

---
+++ mybatisx-core/src/main/java/com/lc/mybatisx/scripting/MybatisxXMLLanguageDriver.java
@@ -0,0 +1,21 @@
+package com.lc.mybatisx.scripting;
+
+import org.apache.ibatis.executor.parameter.ParameterHandler;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.scripting.defaults.DefaultParameterHandler;
+import org.apache.ibatis.scripting.xmltags.XMLLanguageDriver;
+
+/**
+ * @author ：薛承城
+ * @description：一句话描述
+ * @date ：2020/8/17 17:48
+ */
+public class MybatisxXMLLanguageDriver extends XMLLanguageDriver {
+
+    @Override
+    public ParameterHandler createParameterHandler(MappedStatement mappedStatement, Object parameterObject, BoundSql boundSql) {
+        return new DefaultParameterHandler(mappedStatement, parameterObject, boundSql);
+    }
+
+}

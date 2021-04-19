90fe0ec7ea2064246d0a8a6a50ca897212fc22e7
cris <ccxuef@isoftstone.com>
2021-04-19 16:40:04 +0800

    1、增加参考代码

--- mybatisx-core/src/test/java/com/lc/TestLex.java
+++ mybatisx-core/src/test/java/com/lc/TestLex.java
@@ -1,8 +1,11 @@
 package com.lc;
 
+import antlr.ParseTree;
 import org.antlr.runtime.ANTLRInputStream;
 import org.antlr.runtime.CommonTokenStream;
 import org.antlr.runtime.TokenStream;
+import org.antlr.v4.parse.ActionSplitter;
+import org.antlr.v4.parse.ToolANTLRLexer;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -16,11 +19,14 @@ public class TestLex {
 
     @Test
     public void test01() throws IOException {
+        // https://www.jianshu.com/p/21f2afca65e8
+
+        // https://github.com/antlr/grammars-v4/blob/master/jpa/JPA.g4
         String s = "query(std::map .find(x) == y): bla";
         ANTLRInputStream input = new ANTLRInputStream();
-        TokenStream tokens = new CommonTokenStream(new pqlcLexer(input));
+        TokenStream tokens = new CommonTokenStream(new ActionSplitter(input));
 
-        pqlcParser parser = new pqlcParser(tokens);
+        ActionSplitter parser = new ActionSplitter(tokens);
         ParseTree tree = parser.query();
         System.out.println(tree.toStringTree());
     }

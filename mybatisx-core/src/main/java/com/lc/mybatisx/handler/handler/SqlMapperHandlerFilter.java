1bef9c1c935c95ea24119e0ad21c5bcde3512766
cris <ccxuef@isoftstone.com>
2021-07-14 16:19:19 +0800

    策略模式

--- mybatisx-core/src/main/java/com/lc/mybatisx/handler/handler/SqlMapperHandlerFilter.java
+++ mybatisx-core/src/main/java/com/lc/mybatisx/handler/handler/SqlMapperHandlerFilter.java
@@ -1,6 +1,7 @@
 package com.lc.mybatisx.handler.handler;
 
 import com.lc.mybatisx.model.MethodNode;
+import com.lc.mybatisx.syntax.MethodNameParser;
 import org.antlr.v4.runtime.tree.ParseTree;
 
 /**
@@ -12,7 +13,16 @@ public class SqlMapperHandlerFilter implements MapperHandlerFilter {
 
     @Override
     public void doFilter(MethodNode methodNode, ParseTree parseTree, MapperHandlerFilterChain filterChain) {
+        if (parseTree instanceof MethodNameParser.Sql_statementContext) {
+
+        }
+
         String me = methodNode.getName();
+        int childCount = parseTree.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            parseTree.getChild(i);
+        }
+
         filterChain.chain(methodNode, parseTree);
     }
 

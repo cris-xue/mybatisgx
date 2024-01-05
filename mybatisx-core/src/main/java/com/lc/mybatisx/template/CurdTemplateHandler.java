d07a8e3b9306336af778fdd8ec68692c9042ac53
薛承城 <xcc137396549@foxmail.com>
2024-01-05 22:16:53 +0800

    [IMP] 完善新增

--- mybatisx-core/src/main/java/com/lc/mybatisx/template/CurdTemplateHandler.java
+++ mybatisx-core/src/main/java/com/lc/mybatisx/template/CurdTemplateHandler.java
@@ -42,10 +42,7 @@ public class CurdTemplateHandler {
 
     private XNode simpleTemplateHandle(MapperInfo mapperInfo, MethodInfo methodInfo) {
         String methodName = methodInfo.getMethodName();
-        if ("insert".equals(methodName)) {
-            InsertTemplateHandler insertTemplateHandler = new InsertTemplateHandler();
-            return insertTemplateHandler.execute(mapperInfo, methodInfo);
-        } else if ("insertSelective".equals(methodName)) {
+        if ("insert".equals(methodName) || "insertSelective".equals(methodName)) {
             InsertTemplateHandler insertTemplateHandler = new InsertTemplateHandler();
             return insertTemplateHandler.execute(mapperInfo, methodInfo);
         } else {
@@ -53,95 +50,6 @@ public class CurdTemplateHandler {
             Template template = FreeMarkerUtils.getTemplate(templatePath);
             return generateSql(template, mapperInfo, methodInfo);
         }
-    }
-
-    private XNode buildInsertXNode(MapperInfo mapperInfo, MethodInfo methodInfo) {
-        /*InsertNode insertNode = new InsertNode();
-        insertNode.id("");
-        insertNode.keyProperty("");
-        insertNode.useGeneratedKeys(Boolean.TRUE);
-        insertNode.tableName(mapperInfo.getTableName());
-        insertNode.dbColumn(mapperInfo.getResultMapInfo().getColumnInfoList());
-        insertNode.javaColumn(mapperInfo.getResultMapInfo().getColumnInfoList());
-        String insertXmlString = insertNode.getXml();*/
-
-        Document document = DocumentHelper.createDocument();
-        Element mapperElement = document.addElement("mapper");
-        Element insertElement = mapperElement.addElement("insert");
-        insertElement.addAttribute("id", "insert");
-        insertElement.addAttribute("keyProperty", "id");
-        insertElement.addAttribute("useGeneratedKeys", "true");
-        insertElement.addText(String.format("insert into %s", mapperInfo.getTableName()));
-
-        Element dbTrimElement = insertElement.addElement("trim");
-        dbTrimElement.addAttribute("prefix", "(");
-        dbTrimElement.addAttribute("suffix", ")");
-        dbTrimElement.addAttribute("suffixOverrides", ",");
-        mapperInfo.getResultMapInfo().getColumnInfoList().forEach(columnInfo -> {
-            dbTrimElement.addText(String.format("%s, ", columnInfo.getDbColumnName()));
-        });
-
-        Element javaTrimElement = insertElement.addElement("trim");
-        javaTrimElement.addAttribute("prefix", "values (");
-        javaTrimElement.addAttribute("suffix", ")");
-        javaTrimElement.addAttribute("suffixOverrides", ",");
-        mapperInfo.getResultMapInfo().getColumnInfoList().forEach(columnInfo -> {
-            String typeHandler = columnInfo.getTypeHandler();
-            String typeHandlerTemplate = "";
-            if (StringUtils.isNotBlank(typeHandler)) {
-                typeHandlerTemplate = String.format(", typeHandler=%s", typeHandler);
-            }
-            String javaColumn = String.format("#{%s%s}, ", columnInfo.getJavaColumnName(), typeHandlerTemplate);
-            javaTrimElement.addText(javaColumn);
-        });
-
-        String insertXmlString = document.asXML();
-        logger.info(insertXmlString);
-        XPathParser xPathParser = XmlUtils.processXml(insertXmlString);
-        XNode xNode = xPathParser.evalNode("/mapper/insert");
-        return xNode;
-    }
-
-    private XNode buildInsertSelectiveXNode(MapperInfo mapperInfo, MethodInfo methodInfo) {
-        Document document = DocumentHelper.createDocument();
-        Element mapperElement = document.addElement("mapper");
-        Element insertElement = mapperElement.addElement("insert");
-        insertElement.addAttribute("id", "insertSelective");
-        insertElement.addAttribute("keyProperty", "id");
-        insertElement.addAttribute("useGeneratedKeys", "true");
-        insertElement.addText(String.format("insert into %s", mapperInfo.getTableName()));
-
-        Element dbTrimElement = insertElement.addElement("trim");
-        dbTrimElement.addAttribute("prefix", "(");
-        dbTrimElement.addAttribute("suffix", ")");
-        dbTrimElement.addAttribute("suffixOverrides", ",");
-        mapperInfo.getResultMapInfo().getColumnInfoList().forEach(columnInfo -> {
-            Element dbTrimIfElement = dbTrimElement.addElement("if");
-            dbTrimIfElement.addAttribute("test", String.format("%s != null", columnInfo.getJavaColumnName()));
-            dbTrimIfElement.addText(String.format("%s, ", columnInfo.getDbColumnName()));
-        });
-
-        Element javaTrimElement = insertElement.addElement("trim");
-        javaTrimElement.addAttribute("prefix", "values (");
-        javaTrimElement.addAttribute("suffix", ")");
-        javaTrimElement.addAttribute("suffixOverrides", ",");
-        mapperInfo.getResultMapInfo().getColumnInfoList().forEach(columnInfo -> {
-            Element javaTrimIfElement = javaTrimElement.addElement("if");
-            javaTrimIfElement.addAttribute("test", String.format("%s != null", columnInfo.getJavaColumnName()));
-            String typeHandler = columnInfo.getTypeHandler();
-            String typeHandlerTemplate = "";
-            if (StringUtils.isNotBlank(typeHandler)) {
-                typeHandlerTemplate = String.format(", typeHandler=%s", typeHandler);
-            }
-            String javaColumn = String.format("#{%s%s},", columnInfo.getJavaColumnName(), typeHandlerTemplate);
-            javaTrimIfElement.addText(javaColumn);
-        });
-
-        String insertXmlString = document.asXML();
-        logger.info(insertXmlString);
-        XPathParser xPathParser = XmlUtils.processXml(insertXmlString);
-        XNode xNode = xPathParser.evalNode("/mapper/insert");
-        return xNode;
     }
 
     private XNode complexTemplateHandle(MapperInfo mapperInfo, MethodInfo methodInfo) {
--- mybatisx-core/src/main/java/com/lc/mybatisx/template/InsertTemplateHandler.java
+++ mybatisx-core/src/main/java/com/lc/mybatisx/template/InsertTemplateHandler.java
@@ -1,5 +1,8 @@
 package com.lc.mybatisx.template;
 
+import com.lc.mybatisx.annotation.Lock;
+import com.lc.mybatisx.annotation.LogicDelete;
+import com.lc.mybatisx.model.ColumnInfo;
 import com.lc.mybatisx.model.MapperInfo;
 import com.lc.mybatisx.model.MethodInfo;
 import com.lc.mybatisx.utils.XmlUtils;
@@ -12,6 +15,8 @@ import org.dom4j.Element;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+
 public class InsertTemplateHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(InsertTemplateHandler.class);
@@ -21,26 +26,10 @@ public class InsertTemplateHandler {
     }
 
     private XNode simpleTemplateHandle(MapperInfo mapperInfo, MethodInfo methodInfo) {
-        String methodName = methodInfo.getMethodName();
-        if ("insert".equals(methodName)) {
-            return buildInsertXNode(mapperInfo, methodInfo);
-        } else if ("insertSelective".equals(methodName)) {
-            return buildInsertSelectiveXNode(mapperInfo, methodInfo);
-        } else {
-            throw new RuntimeException("暂未实现");
-        }
+        return buildInsertSelectiveXNode(mapperInfo, methodInfo);
     }
 
     private XNode buildInsertXNode(MapperInfo mapperInfo, MethodInfo methodInfo) {
-        /*InsertNode insertNode = new InsertNode();
-        insertNode.id("");
-        insertNode.keyProperty("");
-        insertNode.useGeneratedKeys(Boolean.TRUE);
-        insertNode.tableName(mapperInfo.getTableName());
-        insertNode.dbColumn(mapperInfo.getResultMapInfo().getColumnInfoList());
-        insertNode.javaColumn(mapperInfo.getResultMapInfo().getColumnInfoList());
-        String insertXmlString = insertNode.getXml();*/
-
         Document document = DocumentHelper.createDocument();
         Element mapperElement = document.addElement("mapper");
         Element insertElement = mapperElement.addElement("insert");
@@ -82,7 +71,7 @@ public class InsertTemplateHandler {
         Document document = DocumentHelper.createDocument();
         Element mapperElement = document.addElement("mapper");
         Element insertElement = mapperElement.addElement("insert");
-        insertElement.addAttribute("id", "insertSelective");
+        insertElement.addAttribute("id", methodInfo.getMethodName());
         insertElement.addAttribute("keyProperty", "id");
         insertElement.addAttribute("useGeneratedKeys", "true");
         insertElement.addText(String.format("insert into %s", mapperInfo.getTableName()));
@@ -91,33 +80,109 @@ public class InsertTemplateHandler {
         dbTrimElement.addAttribute("prefix", "(");
         dbTrimElement.addAttribute("suffix", ")");
         dbTrimElement.addAttribute("suffixOverrides", ",");
-        mapperInfo.getResultMapInfo().getColumnInfoList().forEach(columnInfo -> {
-            Element dbTrimIfElement = dbTrimElement.addElement("if");
-            dbTrimIfElement.addAttribute("test", String.format("%s != null", columnInfo.getJavaColumnName()));
-            dbTrimIfElement.addText(String.format("%s, ", columnInfo.getDbColumnName()));
-        });
+
+        List<ColumnInfo> columnInfoList = mapperInfo.getResultMapInfo().getColumnInfoList();
+
+        if (methodInfo.getDynamic()) {
+            for (int i = 0; i < columnInfoList.size(); i++) {
+                ColumnInfo columnInfo = columnInfoList.get(i);
+                String javaColumnName = columnInfo.getJavaColumnName();
+                String dbColumnName = columnInfo.getDbColumnName();
+                Lock lock = columnInfo.getLock();
+                LogicDelete delete = columnInfo.getDelete();
+
+                if (lock != null) {
+                    String javaColumn = String.format("%s, ", dbColumnName);
+                    dbTrimElement.addText(javaColumn);
+                    continue;
+                }
+
+                if (delete != null) {
+                    String javaColumn = String.format("%s, ", dbColumnName);
+                    dbTrimElement.addText(javaColumn);
+                    continue;
+                }
+
+                Element dbTrimIfElement = dbTrimElement.addElement("if");
+                dbTrimIfElement.addAttribute("test", buildTestNotNull(javaColumnName));
+                dbTrimIfElement.addText(String.format("%s, ", dbColumnName));
+            }
+        } else {
+            mapperInfo.getResultMapInfo().getColumnInfoList().forEach(columnInfo -> {
+                dbTrimElement.addText(String.format("%s, ", columnInfo.getDbColumnName()));
+            });
+        }
 
         Element javaTrimElement = insertElement.addElement("trim");
         javaTrimElement.addAttribute("prefix", "values (");
         javaTrimElement.addAttribute("suffix", ")");
         javaTrimElement.addAttribute("suffixOverrides", ",");
-        mapperInfo.getResultMapInfo().getColumnInfoList().forEach(columnInfo -> {
-            Element javaTrimIfElement = javaTrimElement.addElement("if");
-            javaTrimIfElement.addAttribute("test", String.format("%s != null", columnInfo.getJavaColumnName()));
-            String typeHandler = columnInfo.getTypeHandler();
-            String typeHandlerTemplate = "";
-            if (StringUtils.isNotBlank(typeHandler)) {
-                typeHandlerTemplate = String.format(", typeHandler=%s", typeHandler);
+
+        if (methodInfo.getDynamic()) {
+            for (int i = 0; i < columnInfoList.size(); i++) {
+                ColumnInfo columnInfo = columnInfoList.get(i);
+                String typeHandler = columnInfo.getTypeHandler();
+                String javaColumnName = columnInfo.getJavaColumnName();
+                Lock lock = columnInfo.getLock();
+                LogicDelete delete = columnInfo.getDelete();
+
+                if (lock != null) {
+                    String javaColumn = String.format("%s, ", lock.initValue());
+                    javaTrimElement.addText(javaColumn);
+                    continue;
+                }
+
+                if (delete != null) {
+                    String javaColumn = String.format("%s, ", delete.show());
+                    javaTrimElement.addText(javaColumn);
+                    continue;
+                }
+
+                Element javaTrimIfElement = javaTrimElement.addElement("if");
+                javaTrimIfElement.addAttribute("test", buildTestNotNull(javaColumnName));
+                String javaColumn = String.format("#{%s%s},", javaColumnName, buildTypeHandler(typeHandler));
+                javaTrimIfElement.addText(javaColumn);
             }
-            String javaColumn = String.format("#{%s%s},", columnInfo.getJavaColumnName(), typeHandlerTemplate);
-            javaTrimIfElement.addText(javaColumn);
-        });
+        } else {
+            for (int i = 0; i < columnInfoList.size(); i++) {
+                ColumnInfo columnInfo = columnInfoList.get(i);
+                String typeHandler = columnInfo.getTypeHandler();
+                Lock lock = columnInfo.getLock();
+                LogicDelete delete = columnInfo.getDelete();
+
+                if (lock != null) {
+                    String javaColumn = String.format("%s, ", lock.initValue());
+                    javaTrimElement.addText(javaColumn);
+                    continue;
+                }
+
+                if (delete != null) {
+                    String javaColumn = String.format("%s, ", delete.show());
+                    javaTrimElement.addText(javaColumn);
+                    continue;
+                }
+
+                String javaColumn = String.format("#{%s%s}, ", columnInfo.getJavaColumnName(), buildTypeHandler(typeHandler));
+                javaTrimElement.addText(javaColumn);
+            }
+        }
 
         String insertXmlString = document.asXML();
         logger.info(insertXmlString);
         XPathParser xPathParser = XmlUtils.processXml(insertXmlString);
         XNode xNode = xPathParser.evalNode("/mapper/insert");
         return xNode;
+    }
+
+    private String buildTypeHandler(String typeHandler) {
+        if (StringUtils.isNotBlank(typeHandler)) {
+            return String.format(", typeHandler=%s", typeHandler);
+        }
+        return "";
+    }
+
+    private String buildTestNotNull(String javaColumnName) {
+        return String.format("%s != null", javaColumnName);
     }
 
 }

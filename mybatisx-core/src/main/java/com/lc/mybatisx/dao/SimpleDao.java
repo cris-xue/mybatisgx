c5fcef49244345eca9d3b659feca7b7f5d0e50e1
cris <xcc137396549@foxmail.com>
2024-12-08 19:17:19 +0800

    [IMP] 简化dao

--- mybatisx-core/src/main/java/com/lc/mybatisx/dao/SimpleDao.java
+++ mybatisx-core/src/main/java/com/lc/mybatisx/dao/SimpleDao.java
@@ -1,12 +1,36 @@
 package com.lc.mybatisx.dao;
 
+import com.lc.mybatisx.annotation.Dynamic;
+import org.apache.ibatis.annotations.Param;
+
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * @author ：薛承城
  * @description：一句话描述
  * @date ：2020/7/20 14:44
  */
-public interface SimpleDao<ENTITY, ID extends Serializable>
-        extends InsertDao<ENTITY, ID>, UpdateDao<ENTITY, ID>, QueryDao<ENTITY, ID>, DeleteDao<ENTITY, ID> {
+public interface SimpleDao<ENTITY, ID extends Serializable> extends Dao {
+
+    int insert(ENTITY entity);
+
+    @Dynamic
+    int insertSelective(ENTITY entity);
+
+    int deleteById(@Param("id") ID id);
+
+    int updateById(ENTITY entity);
+
+    @Dynamic
+    int updateByIdSelective(ENTITY entity);
+
+    ENTITY findById(@Param("id") ID id);
+
+    @Dynamic
+    ENTITY findOne(ENTITY entity);
+
+    @Dynamic
+    List<ENTITY> findList(ENTITY entity);
+
 }

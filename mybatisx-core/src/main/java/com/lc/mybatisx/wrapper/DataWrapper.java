966457fd9d78c8a48ce0534281e62f6fabb5d4eb
cris <xcc137396549@foxmail.com>
2025-06-10 19:35:24 +0800

    1、删除垃圾代码

--- mybatisx-core/src/main/java/com/lc/mybatisx/wrapper/DataWrapper.java
+++
@@ -1,9 +0,0 @@
-package com.lc.mybatisx.wrapper;
-
-/**
- * @author ：薛承城
- * @description：一句话描述
- * @date ：2021/7/13 14:06
- */
-public class DataWrapper {
-}
--- mybatisx-core/src/main/java/com/lc/mybatisx/wrapper/ResultMapWrapper.java
+++
@@ -1,9 +0,0 @@
-package com.lc.mybatisx.wrapper;
-
-/**
- * @author ：薛承城
- * @description：一句话描述
- * @date ：2021/7/13 13:51
- */
-public class ResultMapWrapper {
-}

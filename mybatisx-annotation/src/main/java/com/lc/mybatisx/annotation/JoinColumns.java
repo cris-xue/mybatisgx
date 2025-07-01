02792b4199ebf5522121c17a46d2cc458b383e25
cris <xcc137396549@foxmail.com>
2025-07-01 12:16:58 +0800

    [IMP] 完善注解

--- mybatisx-annotation/src/main/java/com/lc/mybatisx/annotation/JoinColumns.java
+++ mybatisx-annotation/src/main/java/com/lc/mybatisx/annotation/JoinColumns.java
@@ -1,5 +1,10 @@
 package com.lc.mybatisx.annotation;
 
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
 public @interface JoinColumns {
 
     JoinColumn[] value();

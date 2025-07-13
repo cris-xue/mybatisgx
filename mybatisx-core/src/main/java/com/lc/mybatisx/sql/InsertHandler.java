f8d38f71ac130d1134c1a270361f2ef9d0277fc2
cris <xcc137396549@foxmail.com>
2025-07-13 19:12:23 +0800

    [IMP] 完善添加租户值

--- mybatisx-core/src/main/java/com/lc/mybatisx/sql/InsertHandler.java
+++ mybatisx-core/src/main/java/com/lc/mybatisx/sql/InsertHandler.java
@@ -1,20 +1,23 @@
 package com.lc.mybatisx.sql;
 
+import net.sf.jsqlparser.expression.Expression;
 import net.sf.jsqlparser.expression.StringValue;
-import net.sf.jsqlparser.expression.operators.relational.ExpressionList;
 import net.sf.jsqlparser.schema.Column;
 import net.sf.jsqlparser.statement.insert.Insert;
 import net.sf.jsqlparser.statement.select.LateralSubSelect;
+import net.sf.jsqlparser.statement.select.Select;
+import net.sf.jsqlparser.statement.select.Values;
 
 public class InsertHandler {
 
     public String processInsert(Insert insert, String tenantId) {
         // 添加租户列
         insert.getColumns().add(new Column("tenant_id"));
-        // 添加租户值
-        if (insert.getWithItemsList() instanceof ExpressionList) {
-            ExpressionList list = (ExpressionList) insert.getWithItemsList();
-            list.getExpressions().add(new StringValue(tenantId));
+        Select select = insert.getSelect();
+        if (select instanceof Values) {
+            Values values = (Values) select;
+            Expression expression = new StringValue(tenantId);
+            values.addExpressions(expression);
         } else if (insert.getWithItemsList() instanceof LateralSubSelect) {
             // 处理SELECT类型的INSERT
             // 递归处理子查询

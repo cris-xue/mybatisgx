882c4fefe0177af3ac584d6f880dc571c57f1c62
cris <xcc137396549@foxmail.com>
2025-08-03 17:07:57 +0800

    [ADD] 增加注释

--- mybatisx-annotation/src/main/java/com/lc/mybatisx/annotation/JoinTable.java
+++ mybatisx-annotation/src/main/java/com/lc/mybatisx/annotation/JoinTable.java
@@ -4,15 +4,29 @@ import java.lang.annotation.*;
 
 /**
  * 描述关联字段，用于多对多的关系
+ * @author ccxuef
+ * @date 2025/8/3 17:00
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface JoinTable {
 
+    /**
+     * 关联表名
+     * @return
+     */
     String name();
 
+    /**
+     * 当前实体外键
+     * @return
+     */
     JoinColumn[] joinColumns() default {};
 
+    /**
+     * 关联实体外键
+     * @return
+     */
     JoinColumn[] inverseJoinColumns() default {};
 }

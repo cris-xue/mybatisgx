057f920da8c314cca7ca85d09c39fc7003b00323
cris <xcc137396549@foxmail.com>
2025-08-07 19:13:03 +0800

    [REM] 删除垃圾代码

--- mybatisx-core/src/main/java/com/lc/mybatisx/template/SelectTemplateHandler.java
+++ mybatisx-core/src/main/java/com/lc/mybatisx/template/SelectTemplateHandler.java
@@ -27,12 +27,6 @@ public class SelectTemplateHandler {
         Element selectElement = mapperElement.addElement("select");
         selectElement.addAttribute("id", methodInfo.getMethodName());
         selectElement.addAttribute("resultMap", methodInfo.getResultMapId());
-        // OselectElement.addText("select");
-
-        /*Element dbTrimElement = selectElement.addElement("trim");
-        dbTrimElement.addAttribute("prefix", "");
-        dbTrimElement.addAttribute("suffix", "");
-        dbTrimElement.addAttribute("suffixOverrides", ",");*/
 
         Class<?> methodReturnType = methodInfo.getMethodReturnInfo().getType();
         ResultMapInfo resultMapInfo = mapperInfo.getResultMapInfo(methodReturnType);
@@ -40,27 +34,8 @@ public class SelectTemplateHandler {
 
         SelectSqlTemplateHandler selectSqlTemplateHandler = new SelectSqlTemplateHandler();
         PlainSelect plainSelect = selectSqlTemplateHandler.buildEntityMainSelect(entityInfo);
-
         selectElement.addText(plainSelect.toString());
 
-        /*List<ColumnInfo> tableColumnInfoList = entityInfo.getTableColumnInfoList();
-        for (ColumnInfo columnInfo : tableColumnInfoList) {
-            // 外键不存在，只需要添加字段。外键存在，则需要添加字段和外键
-            ColumnRelationInfo columnRelationInfo = columnInfo.getColumnInfoAnnotationInfo();
-            if (columnRelationInfo == null) {
-                dbTrimElement.addText(String.format("%s, ", columnInfo.getDbColumnName()));
-            } else {
-                ManyToMany manyToMany = columnRelationInfo.getManyToMany();
-                if (manyToMany == null) {
-                    // 只有一对一、一对多、多对一的时候关联字段才需要作为表字段。多对多存在中间表，关联字段在中间中表，不需要作为实体表字段
-                    List<ForeignKeyColumnInfo> inverseForeignKeyColumnInfoList = columnRelationInfo.getInverseForeignKeyColumnInfoList();
-                    for (ForeignKeyColumnInfo inverseForeignKeyColumnInfo : inverseForeignKeyColumnInfoList) {
-                        dbTrimElement.addText(String.format("%s, ", inverseForeignKeyColumnInfo.getName()));
-                    }
-                }
-            }
-        }*/
-        // selectElement.addText(String.format("from %s", entityInfo.getTableName()));
         whereTemplateHandler.execute(selectElement, entityInfo, methodInfo);
         return document.asXML();
     }

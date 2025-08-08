c3a5374fad89fd10b77e8f503490a4495bed7bbc
cris <xcc137396549@foxmail.com>
2025-08-08 17:50:33 +0800

    [IMP] 优化外键信息

--- mybatisx-core/src/main/java/com/lc/mybatisx/model/ForeignKeyColumnInfo.java
+++ mybatisx-core/src/main/java/com/lc/mybatisx/model/ForeignKeyColumnInfo.java
@@ -1,15 +1,28 @@
 package com.lc.mybatisx.model;
 
+/**
+ * 外键字段信息
+ * @author ccxuef
+ * @date 2025/8/8 17:48
+ */
 public class ForeignKeyColumnInfo {
 
     /**
-     * 关联字段
+     * 外键列的名称（当前实体表中的列名）
      */
     private String name;
     /**
-     * 关联字段
+     * 外键列的名称别名
+     */
+    private String nameAlias;
+    /**
+     * 被引用实体的主键列名（目标表的主键列）
      */
     private String referencedColumnName;
+    /**
+     * 被引用实体的主键列名别名
+     */
+    private String referencedColumnNameAlias;
 
     public String getName() {
         return name;
@@ -19,11 +32,27 @@ public class ForeignKeyColumnInfo {
         this.name = name;
     }
 
+    public String getNameAlias() {
+        return nameAlias;
+    }
+
+    public void setNameAlias(String nameAlias) {
+        this.nameAlias = nameAlias;
+    }
+
     public String getReferencedColumnName() {
         return referencedColumnName;
     }
 
     public void setReferencedColumnName(String referencedColumnName) {
         this.referencedColumnName = referencedColumnName;
+    }
+
+    public String getReferencedColumnNameAlias() {
+        return referencedColumnNameAlias;
+    }
+
+    public void setReferencedColumnNameAlias(String referencedColumnNameAlias) {
+        this.referencedColumnNameAlias = referencedColumnNameAlias;
     }
 }

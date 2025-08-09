ff8d517f42c19865e19660de387740567e4b255a
cris <xcc137396549@foxmail.com>
2025-08-09 15:52:17 +0800

    [IMP] 修改非持久化注解

--- mybatisx-annotation/src/main/java/com/lc/mybatisx/annotation/NonPersistent.java
+++ mybatisx-annotation/src/main/java/com/lc/mybatisx/annotation/NonPersistent.java
@@ -7,11 +7,11 @@ import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
- * 属性不映射到数据库表字段
+ * 属性不映射到数据库表字段，非持久化字段
  * @author ccxuef
  * @date 2025/8/9 15:28
  */
 @Target({FIELD})
 @Retention(RUNTIME)
-public @interface Transient {
+public @interface NonPersistent {
 }

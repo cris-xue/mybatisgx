39afc357a021a38a117dea91fdf58a3bdf471b86
cris <xcc137396549@foxmail.com>
2025-10-18 22:20:30 +0800

    [IMP] 优化单元测试

--- mybatisx-core/src/test/java/com/mybatisgx/onetoone/test/UserDetailDaoTest.java
+++ mybatisx-core/src/test/java/com/mybatisgx/onetoone/test/UserDetailDaoTest.java
@@ -1,13 +1,17 @@
 package com.mybatisgx.onetoone.test;
 
 import com.github.swierkosz.fixture.generator.FixtureGenerator;
+import com.mybatisgx.entity.MultiId;
 import com.mybatisgx.onetoone.dao.UserDao;
 import com.mybatisgx.onetoone.dao.UserDetailDao;
 import com.mybatisgx.onetoone.entity.User;
 import com.mybatisgx.onetoone.entity.UserDetail;
+import com.mybatisgx.onetoone.entity.UserDetailItem1;
+import com.mybatisgx.onetoone.entity.UserDetailItem2;
 import com.mybatisgx.util.DaoTestUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -16,36 +20,21 @@ import java.util.List;
 
 public class UserDetailDaoTest {
 
-    @Test
-    public void testFindById() {
-        List<Class<?>> entityClassList = Arrays.asList(User.class, UserDetail.class);
+    private static int count = 10;
+    private static UserDao userDao;
+    private static UserDetailDao userDetailDao;
+
+    @BeforeClass
+    public static void beforeClass() {
+        List<Class<?>> entityClassList = Arrays.asList(User.class, UserDetail.class, UserDetailItem1.class, UserDetailItem2.class);
         List<Class<?>> daoClassList = Arrays.asList(UserDao.class, UserDetailDao.class);
         SqlSession sqlSession = DaoTestUtils.getSqlSession(entityClassList, daoClassList);
-        UserDetailDao userDetailDao = sqlSession.getMapper(UserDetailDao.class);
-
-        FixtureGenerator fixtureGenerator = new FixtureGenerator();
-        fixtureGenerator.configure().ignoreCyclicReferences();
-        UserDetail userDetail = fixtureGenerator.createRandomized(UserDetail.class);
-        int insertCount = userDetailDao.insert(userDetail);
-        Assert.assertEquals(1, insertCount);
-
-        UserDetail dbUserDetail = userDetailDao.findById(userDetail.getMultiId());
-        Assert.assertNotNull(dbUserDetail);
-        Assert.assertEquals(userDetail.getCode(), dbUserDetail.getCode());
-    }
-
-    @Test
-    public void testFindList() {
-        List<Class<?>> entityClassList = Arrays.asList(User.class, UserDetail.class);
-        List<Class<?>> daoClassList = Arrays.asList(UserDao.class, UserDetailDao.class);
-        SqlSession sqlSession = DaoTestUtils.getSqlSession(entityClassList, daoClassList);
-        UserDao userDao = sqlSession.getMapper(UserDao.class);
-        UserDetailDao userDetailDao = sqlSession.getMapper(UserDetailDao.class);
+        userDao = sqlSession.getMapper(UserDao.class);
+        userDetailDao = sqlSession.getMapper(UserDetailDao.class);
 
         FixtureGenerator fixtureGenerator = new FixtureGenerator();
         fixtureGenerator.configure().ignoreCyclicReferences();
 
-        int count = 10;
         List<User> userList = new ArrayList(count);
         List<UserDetail> userDetailList = new ArrayList(count);
         for (int i = 0; i < count; i++) {
@@ -53,14 +42,30 @@ public class UserDetailDaoTest {
             userList.add(user);
 
             UserDetail userDetail = user.getUserDetail();
+            if (i == 0) {
+                MultiId<Long> multiId = new MultiId();
+                multiId.setId1(111111L);
+                multiId.setId2(111111L);
+                userDetail.setMultiId(multiId);
+            }
             userDetail.setUser(user);
-            userDetailList.add(user.getUserDetail());
+            userDetailList.add(userDetail);
         }
-        int insertCount = userDao.insertBatch(userList, count);
-        Assert.assertEquals(count, insertCount);
-        int insertCount1 = userDetailDao.insertBatch(userDetailList, count);
-        Assert.assertEquals(count, insertCount1);
+        userDao.insertBatch(userList, count);
+        userDetailDao.insertBatch(userDetailList, count);
+    }
 
+    @Test
+    public void testFindById() {
+        MultiId<Long> multiId = new MultiId();
+        multiId.setId1(111111L);
+        multiId.setId2(111111L);
+        UserDetail dbUserDetail = userDetailDao.findById(multiId);
+        Assert.assertNotNull(dbUserDetail);
+    }
+
+    @Test
+    public void testFindList() {
         List<UserDetail> dbUserDetailList = userDetailDao.findList(new UserDetail());
         Assert.assertNotNull(dbUserDetailList);
         for (int i = 0; i < count; i++) {
--- mybatisx-core/src/test/resources/db/migration/mysql/V1.0.1__init_complex.sql
+++ mybatisx-core/src/test/resources/db/migration/mysql/V1.0.1__init_complex.sql
@@ -1,7 +1,7 @@
 create table if not exists test_user_complex
 (
-    test_id1       bigint       not null,
-    test_id2       bigint       not null,
+    id1            bigint       not null,
+    id2            bigint       not null,
     role_ids       varchar(256) not null comment '',
     name           varchar(256) null comment '',
     age            int          null comment '',
@@ -16,14 +16,14 @@ create table if not exists test_user_complex
     update_time    datetime     null comment '更新时间',
     status         int          not null comment '',
     version        int          not null comment '',
-    primary key (test_id1)
+    primary key (id1, id2)
 ) engine = InnoDB
   default charset = utf8mb4;
 
 create table if not exists user_detail_complex
 (
-    test_id1       bigint      not null,
-    test_id2       bigint      not null,
+    id1            bigint      not null,
+    id2            bigint      not null,
     code           varchar(64) not null comment '',
     user_id1       bigint      not null comment '',
     user_id2       bigint      not null comment '',
@@ -32,24 +32,56 @@ create table if not exists user_detail_complex
     input_time     datetime    not null comment '输入时间',
     update_user_id bigint      null comment '更新用户id',
     update_time    datetime    null comment '更新时间',
-    primary key (test_id1)
+    primary key (id1, id2)
+) engine = InnoDB
+  default charset = utf8mb4;
+
+create table if not exists user_detail_item1_complex
+(
+    id1             bigint      not null,
+    id2             bigint      not null,
+    code            varchar(64) not null comment '',
+    user_detail_id1 bigint      not null comment '',
+    user_detail_id2 bigint      not null comment '',
+
+    input_user_id   bigint      not null comment '输入用户id',
+    input_time      datetime    not null comment '输入时间',
+    update_user_id  bigint      null comment '更新用户id',
+    update_time     datetime    null comment '更新时间',
+    primary key (id1, id2)
+) engine = InnoDB
+  default charset = utf8mb4;
+
+create table if not exists user_detail_item2_complex
+(
+    id1                  bigint      not null,
+    id2                  bigint      not null,
+    code                 varchar(64) not null comment '',
+    user_detail_item_id1 bigint      not null comment '',
+    user_detail_item_id2 bigint      not null comment '',
+
+    input_user_id        bigint      not null comment '输入用户id',
+    input_time           datetime    not null comment '输入时间',
+    update_user_id       bigint      null comment '更新用户id',
+    update_time          datetime    null comment '更新时间',
+    primary key (id1, id2)
 ) engine = InnoDB
   default charset = utf8mb4;
 
 create table if not exists user_role_complex
 (
-    test_id1 bigint not null,
-    test_id2 bigint not null,
-    user_id  bigint not null comment '',
-    role_id  bigint null comment '',
-    primary key (test_id1)
+    id1     bigint not null,
+    id2     bigint not null,
+    user_id bigint not null comment '',
+    role_id bigint null comment '',
+    primary key (id1, id2)
 ) engine = InnoDB
   default charset = utf8mb4;
 
 create table if not exists role_complex
 (
-    test_id1       bigint      not null,
-    test_id2       bigint      not null,
+    id1            bigint      not null,
+    id2            bigint      not null,
     name           varchar(64) not null comment '',
     code           varchar(64) null comment '',
 
@@ -59,6 +91,6 @@ create table if not exists role_complex
     update_time    datetime    null comment '更新时间',
     status         int         not null comment '',
     version        int         not null comment '',
-    primary key (test_id1)
+    primary key (id1, id2)
 ) engine = InnoDB
   default charset = utf8mb4;

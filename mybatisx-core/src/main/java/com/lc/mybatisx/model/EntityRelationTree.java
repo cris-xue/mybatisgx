c50c80ef150b12c6a6c4d122be7fde03cbb6b9f3
cris <xcc137396549@foxmail.com>
2025-10-25 20:54:48 +0800

    [IMP] 优化实体树模型

--- mybatisx-core/src/main/java/com/lc/mybatisx/model/EntityRelationTree.java
+++ mybatisx-core/src/main/java/com/lc/mybatisx/model/EntityRelationTree.java
@@ -1,23 +1,16 @@
 package com.lc.mybatisx.model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @author ：薛承城
  * @description：实体关系树【user: {roleList: [], orderList: []}】
  * @date ：2021/7/9 17:34
  */
-public class EntityRelationTree extends ColumnEntityRelation {
+public class EntityRelationTree extends ColumnEntityRelation<EntityRelationTree> {
 
     /**
      * 关系层级，第一层为1层，第二层为2层，以此类推，如用户和角色，用户为1、角色为2层
      */
     private int level;
-    /**
-     * 实体关系列表，如用户和订单和角色存在关系，entityRelationList为订单和角色
-     */
-    private List<EntityRelationTree> entityRelationList = new ArrayList();
 
     public int getLevel() {
         return level;
@@ -25,17 +18,5 @@ public class EntityRelationTree extends ColumnEntityRelation {
 
     public void setLevel(int level) {
         this.level = level;
-    }
-
-    public List<EntityRelationTree> getEntityRelationList() {
-        return entityRelationList;
-    }
-
-    public void setEntityRelationList(List<EntityRelationTree> entityRelationList) {
-        this.entityRelationList = entityRelationList;
-    }
-
-    public void addEntityRelation(EntityRelationTree entityRelationInfo) {
-        this.entityRelationList.add(entityRelationInfo);
     }
 }

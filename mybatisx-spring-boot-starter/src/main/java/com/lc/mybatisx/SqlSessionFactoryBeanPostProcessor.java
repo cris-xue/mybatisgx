c8dfe8fb643f2cf0cdaa5faab6cb83fec3e1caf6
cris <xcc137396549@foxmail.com>
2025-10-29 10:10:26 +0800

    [REM] 删除垃圾代码

--- mybatisx-spring-boot-starter/src/main/java/com/lc/mybatisx/SqlSessionFactoryBeanPostProcessor.java
+++ mybatisx-spring-boot-starter/src/main/java/com/lc/mybatisx/SqlSessionFactoryBeanPostProcessor.java
@@ -1,29 +1,14 @@
 package com.lc.mybatisx;
 
 import com.lc.mybatisx.annotation.handler.IdGenerateValueHandler;
-import com.lc.mybatisx.context.MapperInfoContextHolder;
 import com.lc.mybatisx.ext.MybatisxConfiguration;
-import com.lc.mybatisx.ext.MybatisxXMLMapperBuilder;
-import com.lc.mybatisx.model.MapperInfo;
 import com.lc.mybatisx.template.StatementTemplateHandler;
-import com.lc.mybatisx.utils.FreeMarkerUtils;
-import freemarker.template.Template;
 import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.Resource;
-
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class SqlSessionFactoryBeanPostProcessor implements BeanPostProcessor {
 
@@ -62,56 +47,5 @@ public class SqlSessionFactoryBeanPostProcessor implements BeanPostProcessor {
             MybatisxConfiguration mybatisxConfiguration = (MybatisxConfiguration) bean;
         }
         return bean;
-    }
-
-    private void curdMethod(Configuration configuration) {
-        try {
-            List<Resource> mapperResourceList = this.getMapper();
-            for (Resource mapperResource : mapperResourceList) {
-                InputStream is = null;
-                try {
-                    is = mapperResource.getInputStream();
-                    MybatisxXMLMapperBuilder xmlMapperBuilder = new MybatisxXMLMapperBuilder(is,
-                            configuration, mapperResource.toString(), configuration.getSqlFragments());
-                    xmlMapperBuilder.parse();
-                } finally {
-                    if (is != null) {
-                        is.close();
-                    }
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private List<Resource> getMapper() throws IOException {
-        List<Resource> mapperResourceList = new ArrayList<>();
-        List<MapperInfo> mapperInfoList = MapperInfoContextHolder.getMapperInfoList();
-        for (MapperInfo mapperInfo : mapperInfoList) {
-            ByteArrayInputStream bais = null;
-            try {
-                String namespace = mapperInfo.getNamespace();
-                String mapperXml = createMapperXml(namespace);
-                bais = new ByteArrayInputStream(mapperXml.getBytes());
-                Resource resource = new InputStreamResource(bais, namespace);
-                mapperResourceList.add(resource);
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                if (bais != null) {
-                    bais.close();
-                }
-            }
-        }
-        return mapperResourceList;
-    }
-
-    private String createMapperXml(String namespace) {
-        Template template = FreeMarkerUtils.getTemplate("mapper/base.ftl");
-        Map<String, Object> templateData = new HashMap<>();
-        templateData.put("namespace", namespace);
-        String baseXml = FreeMarkerUtils.processTemplate(templateData, template);
-        return baseXml;
     }
 }

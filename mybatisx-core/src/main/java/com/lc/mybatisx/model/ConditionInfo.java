00685a424c8c02b50041ebd6aa41688bba21048a
cris <xcc137396549@foxmail.com>
2025-11-08 17:47:45 +0800

    [ADD] 增加参数获取公共路径字段

--- mybatisx-core/src/main/java/com/lc/mybatisx/model/ConditionInfo.java
+++ mybatisx-core/src/main/java/com/lc/mybatisx/model/ConditionInfo.java
@@ -1,5 +1,7 @@
 package com.lc.mybatisx.model;
 
+import java.util.List;
+
 public class ConditionInfo {
 
     /**
@@ -48,6 +50,10 @@ public class ConditionInfo {
      * 条件组信息，一个条件组包含多个条件，一个条件可能由一个条件组或者一个条件字段组成
      */
     private ConditionGroupInfo conditionGroupInfo;
+    /**
+     * 条件值来源于方法参数或者方法参数实体，需要提前计算出公共取值路径，模板渲染的时候就不再需要多重逻辑判断
+     */
+    private List<String> paramValueCommonPathItemList;
 
     public ConditionInfo(Integer index, ConditionOriginType conditionOriginType) {
         this.index = index;
@@ -146,5 +152,13 @@ public class ConditionInfo {
 
     public void setConditionGroupInfo(ConditionGroupInfo conditionGroupInfo) {
         this.conditionGroupInfo = conditionGroupInfo;
+    }
+
+    public List<String> getParamValueCommonPathItemList() {
+        return paramValueCommonPathItemList;
+    }
+
+    public void setParamValueCommonPathItemList(List<String> paramValueCommonPathItemList) {
+        this.paramValueCommonPathItemList = paramValueCommonPathItemList;
     }
 }

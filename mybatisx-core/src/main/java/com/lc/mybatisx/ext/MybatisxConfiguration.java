197d08c6f72a418ca5aaa2f957bc0d8c718992bd
cris <xcc137396549@foxmail.com>
2025-11-10 19:24:24 +0800

    [REM] 删除废弃代码

--- mybatisx-core/src/main/java/com/lc/mybatisx/ext/MybatisxConfiguration.java
+++ mybatisx-core/src/main/java/com/lc/mybatisx/ext/MybatisxConfiguration.java
@@ -1,17 +1,8 @@
 package com.lc.mybatisx.ext;
 
-import com.lc.mybatisx.annotation.GenerateValue;
-import com.lc.mybatisx.annotation.Id;
-import com.lc.mybatisx.annotation.TenantId;
-import com.lc.mybatisx.annotation.handler.GenerateValueHandler;
 import com.lc.mybatisx.annotation.handler.IdGenerateValueHandler;
-import com.lc.mybatisx.annotation.handler.JavaColumnInfo;
-import com.lc.mybatisx.context.EntityInfoContextHolder;
 import com.lc.mybatisx.ext.executor.MybatisgxBatchExecutor;
 import com.lc.mybatisx.ext.executor.MybatisgxMixExecutor;
-import com.lc.mybatisx.ext.mapping.BatchSelectBoundSql;
-import com.lc.mybatisx.model.ColumnInfo;
-import com.lc.mybatisx.model.EntityInfo;
 import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.executor.parameter.ParameterHandler;
 import org.apache.ibatis.executor.resultset.ResultSetHandler;
@@ -19,19 +10,13 @@ import org.apache.ibatis.executor.statement.StatementHandler;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.Environment;
 import org.apache.ibatis.mapping.MappedStatement;
-import org.apache.ibatis.mapping.SqlCommandType;
-import org.apache.ibatis.reflection.MetaObject;
 import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.ExecutorType;
 import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.transaction.Transaction;
 
-import java.util.List;
-
 public class MybatisxConfiguration extends Configuration {
-
-    private IdGenerateValueHandler<?> idGenerateValueHandler;
 
     public MybatisxConfiguration() {
         super();
@@ -42,7 +27,6 @@ public class MybatisxConfiguration extends Configuration {
     }
 
     public void setIdGenerateValueHandler(IdGenerateValueHandler<?> idGenerateValueHandler) {
-        this.idGenerateValueHandler = idGenerateValueHandler;
     }
 
     @Override
@@ -70,70 +54,5 @@ public class MybatisxConfiguration extends Configuration {
         resultSetHandler = new MybatisgxResultSetHandler(executor, mappedStatement, parameterHandler, resultHandler, boundSql, rowBounds);
         resultSetHandler = (ResultSetHandler) interceptorChain.pluginAll(resultSetHandler);
         return resultSetHandler;
-    }
-
-    private Object fillParameterObject(MappedStatement mappedStatement, Object parameterObject) {
-        if (parameterObject == null) {
-            return null;
-        }
-
-        SqlCommandType sqlCommandType = mappedStatement.getSqlCommandType();
-        EntityInfo entityInfo = EntityInfoContextHolder.get(parameterObject.getClass());
-        boolean isFill = (SqlCommandType.INSERT == sqlCommandType || SqlCommandType.UPDATE == sqlCommandType) && entityInfo != null;
-        if (!isFill) {
-            return parameterObject;
-        }
-
-        MetaObject metaObject = super.newMetaObject(parameterObject);
-        List<ColumnInfo> generateValueColumnInfoList = entityInfo.getGenerateValueColumnInfoList();
-        for (ColumnInfo generateValueColumnInfo : generateValueColumnInfoList) {
-            String javaColumnName = generateValueColumnInfo.getJavaColumnName();
-            Class<?> javaColumnType = metaObject.getSetterType(javaColumnName);
-            Object originalValue = metaObject.getValue(javaColumnName);
-
-            Object value = this.next(sqlCommandType, generateValueColumnInfo, javaColumnType, originalValue);
-            metaObject.setValue(javaColumnName, value);
-        }
-        return metaObject.getOriginalObject();
-    }
-
-    private Object next(SqlCommandType sqlCommandType, ColumnInfo columnInfo, Class<?> javaColumnType, Object originalValue) {
-        JavaColumnInfo javaColumnInfo = new JavaColumnInfo();
-        javaColumnInfo.setType(javaColumnType);
-        javaColumnInfo.setColumnName(columnInfo.getJavaColumnName());
-        javaColumnInfo.setId(columnInfo.getId());
-        javaColumnInfo.setLock(columnInfo.getLock());
-        javaColumnInfo.setLogicDelete(columnInfo.getLogicDelete());
-        javaColumnInfo.setGenerateValue(columnInfo.getGenerateValue());
-
-        GenerateValue generateValue = columnInfo.getGenerateValue();
-        if (generateValue != null) {
-            GenerateValueHandler generateValueHandler = columnInfo.getGenerateValueHandler();
-            Boolean insert = generateValue.insert();
-            if (insert && sqlCommandType == SqlCommandType.INSERT) {
-                return generateValueHandler.insert(javaColumnInfo, originalValue);
-            }
-            Boolean update = generateValue.update();
-            if (update && sqlCommandType == SqlCommandType.UPDATE) {
-                return generateValueHandler.update(javaColumnInfo, originalValue);
-            }
-        }
-        Id id = columnInfo.getId();
-        if (id != null && sqlCommandType == SqlCommandType.INSERT) {
-            return this.idGenerateValueHandler.insert(javaColumnInfo, originalValue);
-        }
-        TenantId tenantId = columnInfo.getTenantId();
-        if (tenantId != null) {
-            return this.idGenerateValueHandler.insert(javaColumnInfo, originalValue);
-        }
-        return originalValue;
-    }
-
-    public Executor getExecutor(Transaction transaction, ExecutorType executorType) {
-        Executor executor = super.newExecutor(transaction, executorType);
-        if (executorType == ExecutorType.BATCH) {
-            return new MybatisgxBatchExecutor(executor);
-        }
-        return executor;
     }
 }
--- mybatisx-core/src/main/java/com/lc/mybatisx/ext/MybatisxXMLLanguageDriver.java
+++ mybatisx-core/src/main/java/com/lc/mybatisx/ext/MybatisxXMLLanguageDriver.java
@@ -1,23 +1,10 @@
 package com.lc.mybatisx.ext;
 
-import com.lc.mybatisx.annotation.GenerateValue;
-import com.lc.mybatisx.annotation.Id;
-import com.lc.mybatisx.annotation.handler.GenerateValueHandler;
-import com.lc.mybatisx.annotation.handler.IdGenerateValueHandler;
-import com.lc.mybatisx.annotation.handler.JavaColumnInfo;
-import com.lc.mybatisx.context.EntityInfoContextHolder;
-import com.lc.mybatisx.model.ColumnInfo;
-import com.lc.mybatisx.model.EntityInfo;
 import org.apache.ibatis.executor.parameter.ParameterHandler;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
-import org.apache.ibatis.mapping.SqlCommandType;
-import org.apache.ibatis.reflection.MetaObject;
 import org.apache.ibatis.scripting.defaults.DefaultParameterHandler;
 import org.apache.ibatis.scripting.xmltags.XMLLanguageDriver;
-import org.apache.ibatis.session.Configuration;
-
-import java.util.List;
 
 /**
  * @author ：薛承城
@@ -26,72 +13,8 @@ import java.util.List;
  */
 public class MybatisxXMLLanguageDriver extends XMLLanguageDriver {
 
-    private IdGenerateValueHandler<?> idGenerateValueHandler;
-
     @Override
     public ParameterHandler createParameterHandler(MappedStatement mappedStatement, Object parameterObject, BoundSql boundSql) {
-        parameterObject = this.fillParameterObject(mappedStatement, parameterObject);
         return new DefaultParameterHandler(mappedStatement, parameterObject, boundSql);
     }
-
-    protected Object fillParameterObject(MappedStatement mappedStatement, Object parameterObject) {
-        if (parameterObject == null) {
-            return null;
-        }
-
-        SqlCommandType sqlCommandType = mappedStatement.getSqlCommandType();
-        EntityInfo entityInfo = EntityInfoContextHolder.get(parameterObject.getClass());
-        boolean isFill = (SqlCommandType.INSERT == sqlCommandType || SqlCommandType.UPDATE == sqlCommandType) && entityInfo != null;
-        if (!isFill) {
-            return parameterObject;
-        }
-
-        Configuration configuration = mappedStatement.getConfiguration();
-        MetaObject metaObject = configuration.newMetaObject(parameterObject);
-
-        List<ColumnInfo> generateValueColumnInfoList = entityInfo.getGenerateValueColumnInfoList();
-        for (ColumnInfo generateValueColumnInfo : generateValueColumnInfoList) {
-            String javaColumnName = generateValueColumnInfo.getJavaColumnName();
-            Class<?> javaColumnType = metaObject.getSetterType(javaColumnName);
-            Object originalValue = metaObject.getValue(javaColumnName);
-
-            Object value = this.next(sqlCommandType, generateValueColumnInfo, javaColumnType, originalValue);
-            // Object castValue = javaColumnType.cast(value);
-            metaObject.setValue(javaColumnName, value);
-        }
-        return metaObject.getOriginalObject();
-    }
-
-    private Object next(SqlCommandType sqlCommandType, ColumnInfo columnInfo, Class<?> javaColumnType, Object originalValue) {
-        JavaColumnInfo javaColumnInfo = new JavaColumnInfo();
-        javaColumnInfo.setType(javaColumnType);
-        javaColumnInfo.setColumnName(columnInfo.getJavaColumnName());
-        javaColumnInfo.setId(columnInfo.getId());
-        javaColumnInfo.setLock(columnInfo.getLock());
-        javaColumnInfo.setLogicDelete(columnInfo.getLogicDelete());
-        javaColumnInfo.setGenerateValue(columnInfo.getGenerateValue());
-
-        GenerateValue generateValue = columnInfo.getGenerateValue();
-        if (generateValue != null) {
-            GenerateValueHandler generateValueHandler = columnInfo.getGenerateValueHandler();
-            Boolean insert = generateValue.insert();
-            if (insert && sqlCommandType == SqlCommandType.INSERT) {
-                return generateValueHandler.insert(javaColumnInfo, originalValue);
-            }
-            Boolean update = generateValue.update();
-            if (update && sqlCommandType == SqlCommandType.UPDATE) {
-                return generateValueHandler.update(javaColumnInfo, originalValue);
-            }
-        }
-        Id id = columnInfo.getId();
-        if (id != null && sqlCommandType == SqlCommandType.INSERT) {
-            return this.idGenerateValueHandler.insert(javaColumnInfo, originalValue);
-        }
-        return originalValue;
-    }
-
-    public void setIdGenerateValueHandler(IdGenerateValueHandler<?> idGenerateValueHandler) {
-        this.idGenerateValueHandler = idGenerateValueHandler;
-    }
-
 }

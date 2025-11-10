ce5ce39170a93d5299a3da02f6f537ee041134c0
cris <xcc137396549@foxmail.com>
2025-11-10 18:50:02 +0800

    [REM] 删除测试工程

--- mybatisx-test/src/main/java/com/lc/mybatisx/test/dao/OrgDao.java
+++
@@ -1,9 +0,0 @@
-package com.lc.mybatisx.test.dao;
-
-import com.lc.mybatisx.dao.SimpleDao;
-import com.lc.mybatisx.test.model.entity.Org;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrgDao extends SimpleDao<Org, Long> {
-}

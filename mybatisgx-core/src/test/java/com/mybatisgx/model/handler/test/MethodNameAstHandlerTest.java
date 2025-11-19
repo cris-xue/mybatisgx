41c097ff3e18f44e70b0e3c4db08f87fc7f7a47e
cris <xcc137396549@foxmail.com>
2025-11-19 18:20:23 +0800

    [IMP] 修改单元测试

--- mybatisgx-core/src/test/java/com/mybatisgx/model/handler/test/MethodNameAstHandlerTest.java
+++ mybatisgx-core/src/test/java/com/mybatisgx/model/handler/test/MethodNameAstHandlerTest.java
@@ -5,7 +5,7 @@ import com.mybatisgx.model.ConditionOriginType;
 import com.mybatisgx.model.EntityInfo;
 import com.mybatisgx.model.MethodInfo;
 import com.mybatisgx.model.handler.EntityInfoHandler;
-import com.mybatisgx.model.handler.MybatisgxSyntaxHandler;
+import com.mybatisgx.model.handler.MybatisgxSyntaxProcessor;
 import org.apache.ibatis.mapping.SqlCommandType;
 import org.junit.Assert;
 import org.junit.Test;
@@ -15,15 +15,23 @@ import java.util.List;
 public class MethodNameAstHandlerTest {
 
     private EntityInfoHandler entityInfoHandler = new EntityInfoHandler();
-    private MybatisgxSyntaxHandler mybatisgxSyntaxHandler = new MybatisgxSyntaxHandler();
+
+    private MybatisgxSyntaxProcessor buildProcessor() {
+        return new MybatisgxSyntaxProcessor();
+    }
 
     @Test
     public void test01() {
         // 测试方法名中只有条件
         EntityInfo entityInfo = entityInfoHandler.execute(User.class);
         MethodInfo methodInfo = new MethodInfo();
-        mybatisgxSyntaxHandler.execute(entityInfo, methodInfo, null, ConditionOriginType.METHOD_NAME, "findByName");
-
+        MybatisgxSyntaxProcessor processor = this.buildProcessor();
+        processor.execute(
+                entityInfo,
+                methodInfo,
+                null,
+                ConditionOriginType.METHOD_NAME,
+                "findByName");
         Assert.assertEquals(SqlCommandType.SELECT, methodInfo.getSqlCommandType());
         List<ConditionInfo> conditionInfoList = methodInfo.getConditionInfoList();
         Assert.assertTrue(conditionInfoList != null && conditionInfoList.size() > 0);
@@ -36,7 +44,14 @@ public class MethodNameAstHandlerTest {
         // 测试方法名中存在多条件
         EntityInfo entityInfo = entityInfoHandler.execute(User.class);
         MethodInfo methodInfo = new MethodInfo();
-        mybatisgxSyntaxHandler.execute(entityInfo, methodInfo, null, ConditionOriginType.METHOD_NAME, "findByIdAndNameOrNameEqAndNameLike");
+        MybatisgxSyntaxProcessor processor = this.buildProcessor();
+        processor.execute(
+                entityInfo,
+                methodInfo,
+                null,
+                ConditionOriginType.METHOD_NAME,
+                "findByIdAndNameOrNameEqAndNameLike"
+        );
 
         Assert.assertEquals(SqlCommandType.SELECT, methodInfo.getSqlCommandType());
         List<ConditionInfo> conditionInfoList = methodInfo.getConditionInfoList();
@@ -48,13 +63,13 @@ public class MethodNameAstHandlerTest {
 
         ConditionInfo conditionInfo2 = conditionInfoList.get(1);
         Assert.assertEquals("name", conditionInfo2.getColumnName());
-        Assert.assertEquals("AndName", conditionInfo2.getOriginSegment());
+        Assert.assertEquals("Name", conditionInfo2.getOriginSegment());
 
         ConditionInfo conditionInfo3 = conditionInfoList.get(2);
-        Assert.assertEquals("OrNameEq", conditionInfo3.getOriginSegment());
+        Assert.assertEquals("NameEq", conditionInfo3.getOriginSegment());
 
         ConditionInfo conditionInfo4 = conditionInfoList.get(3);
-        Assert.assertEquals("AndNameLike", conditionInfo4.getOriginSegment());
+        Assert.assertEquals("NameLike", conditionInfo4.getOriginSegment());
     }
 
     @Test(expected = RuntimeException.class)
@@ -62,6 +77,13 @@ public class MethodNameAstHandlerTest {
         // 测试方法名中的条件在实体中不存在
         EntityInfo entityInfo = entityInfoHandler.execute(User.class);
         MethodInfo methodInfo = new MethodInfo();
-        mybatisgxSyntaxHandler.execute(entityInfo, methodInfo, null, ConditionOriginType.METHOD_NAME, "findByName1");
+        MybatisgxSyntaxProcessor processor = this.buildProcessor();
+        processor.execute(
+                entityInfo,
+                methodInfo,
+                null,
+                ConditionOriginType.METHOD_NAME,
+                "findByName1"
+        );
     }
 }

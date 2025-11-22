06adac083f58ca4e79800421272e987ab97a3efa
cris <xcc137396549@foxmail.com>
2025-11-22 19:59:21 +0800

    [REM] 优化语法

--- mybatisgx-core/src/main/java/com/mybatisgx/syntax/MethodNameLexer.java
+++ mybatisgx-core/src/main/java/com/mybatisgx/syntax/MethodNameLexer.java
@@ -93,36 +93,6 @@ public class MethodNameLexer extends Lexer {
 	}
 
 
-	    private boolean afterSelect = false;
-	    private boolean afterBy = false;
-
-	    @Override
-	    public Token emit() {
-	        Token token = super.emit();
-	        switch(token.getType()) {
-	            case SELECT_COLUMN_ACTION:
-	                afterSelect = true;
-	                afterBy = false;
-	                break;
-	            case SELECT_COUNT_ACTION:
-	                afterSelect = true;
-	                afterBy = false;
-	                break;
-	            case BY:
-	                afterSelect = true;
-	                afterBy = true;
-	                break;
-	            default:
-	                // 其它行为
-	        }
-	        return token;
-	    }
-
-	    public boolean isIgnore() {
-	        return afterSelect && afterBy;
-	    }
-
-
 	public MethodNameLexer(CharStream input) {
 		super(input);
 		_interp = new LexerATNSimulator(this,_ATN,_decisionToDFA,_sharedContextCache);

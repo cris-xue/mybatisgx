520dbeaecedf60456fa1c665cf5d7c8e9e970bbc
cris <xcc137396549@foxmail.com>
2025-11-26 12:40:28 +0800

    [IMP] 优化单元测试代码

--- mybatisgx-core/src/test/java/com/mybatisgx/util/DaoTestUtils.java
+++ mybatisgx-core/src/test/java/com/mybatisgx/util/DaoTestUtils.java
@@ -1,21 +1,17 @@
 package com.mybatisgx.util;
 
 import com.alibaba.druid.pool.DruidDataSource;
-import com.github.pagehelper.PageInterceptor;
-import com.google.common.collect.Sets;
 import com.mybatisgx.context.MybatisgxContextLoader;
+import com.mybatisgx.context.MybatisgxObjectFactory;
 import com.mybatisgx.ext.builder.xml.MybatisgxXMLConfigBuilder;
 import com.mybatisgx.ext.session.MybatisgxConfiguration;
 import com.mybatisgx.ext.session.defaults.MybatisgxDefaultSqlSessionFactory;
 import com.mybatisgx.template.StatementTemplateHandler;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.ibatis.logging.stdout.StdOutImpl;
-import org.apache.ibatis.mapping.Environment;
 import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.defaults.DefaultSqlSessionFactory;
-import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
 import org.flywaydb.core.Flyway;
 import org.springframework.core.io.ClassPathResource;
 
@@ -47,51 +43,26 @@ public class DaoTestUtils {
         flyway.migrate();
     }
 
-    protected static DataSource dataSource() {
-        return dataSource(properties.getProperty("dataSourceUrl"));
-    }
-
-    protected static DataSource dataSource(String url) {
-        DruidDataSource dataSource = new DruidDataSource();
-        dataSource.setUrl(url);
-        dataSource.setUsername("sa");
-        dataSource.setPassword("");
-        dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setDefaultAutoCommit(false);
-        return dataSource;
-    }
-
     protected static MybatisgxConfiguration context(String[] entityBasePackages, String[] daoBasePackages) {
-        DataSource dataSource = dataSource();
-        flywayInit(dataSource);
-
         Configuration configuration;
         try {
             ClassPathResource classPathResource = new ClassPathResource("mybatis-config.xml");
             MybatisgxXMLConfigBuilder xmlConfigBuilder = new MybatisgxXMLConfigBuilder(classPathResource.getInputStream());
             configuration = xmlConfigBuilder.parse();
+            MybatisgxObjectFactory.register((MybatisgxConfiguration) configuration);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        Environment environment = new Environment.Builder("test_env")
-                .transactionFactory(new JdbcTransactionFactory())
-                .dataSource(dataSource)
-                .build();
-        MybatisgxConfiguration mybatisgxConfiguration = new MybatisgxConfiguration(environment);
-        mybatisgxConfiguration.setLogImpl(StdOutImpl.class);
-        mybatisgxConfiguration.setLazyLoadingEnabled(true);
-        mybatisgxConfiguration.setAggressiveLazyLoading(false);
-        mybatisgxConfiguration.setLazyLoadTriggerMethods(Sets.newHashSet("get"));
-        mybatisgxConfiguration.addInterceptor(new PageInterceptor());
-        mybatisgxConfiguration.setDatabaseId(DB_TYPE);
+        DataSource dataSource = configuration.getEnvironment().getDataSource();
+        flywayInit(dataSource);
 
         MybatisgxContextLoader mybatisgxContextLoader = new MybatisgxContextLoader(entityBasePackages, daoBasePackages, null);
         mybatisgxContextLoader.load();
 
         StatementTemplateHandler statementTemplateHandler = new StatementTemplateHandler((MybatisgxConfiguration) configuration);
-        statementTemplateHandler.curdMethod(mybatisgxConfiguration);
-        return mybatisgxConfiguration;
+        statementTemplateHandler.curdMethod(configuration);
+        return (MybatisgxConfiguration) configuration;
     }
 
     public static SqlSession getSqlSession(String[] entityBasePackages, String[] daoBasePackages) {

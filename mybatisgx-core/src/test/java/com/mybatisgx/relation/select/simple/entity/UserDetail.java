1df09be147c65aa3cd07c3602d145cc7a0ee3f9f
cris <xcc137396549@foxmail.com>
2025-11-29 14:18:49 +0800

    [ADD] 增加简单关联查询单元测试

---
+++ mybatisgx-core/src/test/java/com/mybatisgx/relation/select/simple/entity/UserDetail.java
@@ -0,0 +1,37 @@
+package com.mybatisgx.relation.select.simple.entity;
+
+import com.mybatisgx.annotation.*;
+import com.mybatisgx.entity.EmbeddedIdBaseEntity;
+
+import javax.persistence.FetchType;
+
+@Entity
+@Table(name = "user_detail_complex")
+public class UserDetail extends EmbeddedIdBaseEntity<Long> {
+
+    private String code;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumns({
+            @JoinColumn(name = "user_id1", referencedColumnName = "id1"),
+            @JoinColumn(name = "user_id2", referencedColumnName = "id2")
+    })
+    @Fetch(FetchMode.SIMPLE)
+    private User user;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}

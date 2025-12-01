dfaf7b953678c5477f3231d3f745ecf632552511
cris <xcc137396549@foxmail.com>
2025-12-01 14:34:30 +0800

    [IMP] 优化与spring boot集成代码

--- mybatisgx-spring-boot-starter/src/main/java/com/mybatisgx/boot/MybatisgxRegistrar.java
+++ mybatisgx-spring-boot-starter/src/main/java/com/mybatisgx/boot/MybatisgxRegistrar.java
@@ -1,27 +1,11 @@
 package com.mybatisgx.boot;
 
-import com.mybatisgx.context.MybatisgxContextLoader;
-import com.mybatisgx.dao.Dao;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.config.ConstructorArgumentValues;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.GenericBeanDefinition;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
 import org.springframework.core.annotation.AnnotationAttributes;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.core.type.AnnotationMetadata;
-import org.springframework.core.type.ClassMetadata;
-import org.springframework.core.type.classreading.CachingMetadataReaderFactory;
-import org.springframework.core.type.classreading.MetadataReaderFactory;
-import org.springframework.stereotype.Repository;
-import org.springframework.util.ClassUtils;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author ：薛承城
@@ -29,11 +13,6 @@ import java.util.List;
  * @date ：2020/1/8 18:06
  */
 public class MybatisgxRegistrar implements ImportBeanDefinitionRegistrar {
-
-    private static final Logger logger = LoggerFactory.getLogger(MybatisgxRegistrar.class);
-
-    private static final ResourcePatternResolver RESOURCE_PATTERN_RESOLVER = new PathMatchingResourcePatternResolver();
-    private static final MetadataReaderFactory METADATA_READER_FACTORY = new CachingMetadataReaderFactory();
 
     @Override
     public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry) {
@@ -43,40 +22,14 @@ public class MybatisgxRegistrar implements ImportBeanDefinitionRegistrar {
         if (annotationAttributes != null) {
             String[] entityBasePackages = (String[]) annotationAttributes.get("entityBasePackages");
             String[] daoBasePackages = (String[]) annotationAttributes.get("daoBasePackages");
-            List<Resource> resourceList = this.getDaoResourceList(daoBasePackages);
-            MybatisgxContextLoader mybatisgxContextLoader = new MybatisgxContextLoader(entityBasePackages, daoBasePackages, resourceList);
 
             ConstructorArgumentValues constructorArgumentValues = new ConstructorArgumentValues();
-            constructorArgumentValues.addIndexedArgumentValue(0, mybatisgxContextLoader);
+            constructorArgumentValues.addIndexedArgumentValue(0, entityBasePackages);
+            constructorArgumentValues.addIndexedArgumentValue(1, daoBasePackages);
             GenericBeanDefinition sqlSessionFactoryBeanPostProcessorBeanDefinition = new GenericBeanDefinition();
             sqlSessionFactoryBeanPostProcessorBeanDefinition.setBeanClass(SqlSessionFactoryBeanPostProcessor.class);
             sqlSessionFactoryBeanPostProcessorBeanDefinition.setConstructorArgumentValues(constructorArgumentValues);
             registry.registerBeanDefinition("sqlSessionFactoryBeanPostProcessorBeanDefinition", sqlSessionFactoryBeanPostProcessorBeanDefinition);
         }
-    }
-
-    private List<Resource> getDaoResourceList(String[] basePackages) {
-        List<Resource> resourceList = new ArrayList();
-        try {
-            for (String basePackage : basePackages) {
-                basePackage = ResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX + basePackage.replace('.', '/') + "/**.class";
-                ClassUtils.convertClassNameToResourcePath(basePackage).concat("/**.class");
-                Resource[] resources = RESOURCE_PATTERN_RESOLVER.getResources(basePackage);
-                for (Resource resource : resources) {
-                    ClassMetadata classMetadata = METADATA_READER_FACTORY.getMetadataReader(resource).getClassMetadata();
-                    Class<?> clazz = Class.forName(classMetadata.getClassName());
-                    Class<?> daoType = Dao.class;
-                    Repository repository = clazz.getAnnotation(Repository.class);
-                    if (daoType.isAssignableFrom(clazz) && repository != null) {
-                        resourceList.add(resource);
-                    }
-                }
-            }
-        } catch (IOException e) {
-            logger.error(e.getMessage(), e);
-        } catch (ClassNotFoundException e) {
-            logger.error(e.getMessage(), e);
-        }
-        return resourceList;
     }
 }
--- mybatisgx-spring-boot-starter/src/main/java/com/mybatisgx/boot/SqlSessionFactoryBeanPostProcessor.java
+++ mybatisgx-spring-boot-starter/src/main/java/com/mybatisgx/boot/SqlSessionFactoryBeanPostProcessor.java
@@ -2,6 +2,7 @@ package com.mybatisgx.boot;
 
 import com.mybatisgx.annotation.handler.IdGenerateValueHandler;
 import com.mybatisgx.context.MybatisgxContextLoader;
+import com.mybatisgx.dao.Dao;
 import com.mybatisgx.ext.session.MybatisgxConfiguration;
 import com.mybatisgx.template.StatementTemplateHandler;
 import org.apache.ibatis.session.Configuration;
@@ -10,17 +11,34 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.core.type.ClassMetadata;
+import org.springframework.core.type.classreading.CachingMetadataReaderFactory;
+import org.springframework.core.type.classreading.MetadataReaderFactory;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.ClassUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SqlSessionFactoryBeanPostProcessor implements BeanPostProcessor {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SqlSessionFactoryBeanPostProcessor.class);
 
-    private MybatisgxContextLoader mybatisgxContextLoader;
+    private static final ResourcePatternResolver RESOURCE_PATTERN_RESOLVER = new PathMatchingResourcePatternResolver();
+    private static final MetadataReaderFactory METADATA_READER_FACTORY = new CachingMetadataReaderFactory();
+
+    private final String[] entityBasePackages;
+    private final String[] daoBasePackages;
     // @Autowired
     private IdGenerateValueHandler<?> idGenerateValueHandler;
 
-    public SqlSessionFactoryBeanPostProcessor(MybatisgxContextLoader mybatisgxContextLoader) {
-        this.mybatisgxContextLoader = mybatisgxContextLoader;
+    public SqlSessionFactoryBeanPostProcessor(String[] entityBasePackages, String[] daoBasePackages) {
+        this.entityBasePackages = entityBasePackages;
+        this.daoBasePackages = daoBasePackages;
     }
 
     @Override
@@ -40,12 +58,14 @@ public class SqlSessionFactoryBeanPostProcessor implements BeanPostProcessor {
             if (configuration instanceof MybatisgxConfiguration) {
                 MybatisgxConfiguration mybatisgxConfiguration = (MybatisgxConfiguration) configuration;
                 mybatisgxConfiguration.setIdGenerateValueHandler(idGenerateValueHandler);
+
+                List<Resource> resourceList = this.getDaoResourceList(daoBasePackages);
+                MybatisgxContextLoader mybatisgxContextLoader = new MybatisgxContextLoader(entityBasePackages, daoBasePackages, resourceList, mybatisgxConfiguration);
+                mybatisgxContextLoader.load();
+
+                StatementTemplateHandler statementTemplateHandler = new StatementTemplateHandler((MybatisgxConfiguration) configuration);
+                statementTemplateHandler.curdMethod(configuration);
             }
-
-            mybatisgxContextLoader.load();
-
-            StatementTemplateHandler statementTemplateHandler = new StatementTemplateHandler((MybatisgxConfiguration) configuration);
-            statementTemplateHandler.curdMethod(configuration);
         }
         if (bean instanceof MybatisgxProperties) {
             MybatisgxProperties mybatisxProperties = (MybatisgxProperties) bean;
@@ -54,5 +74,30 @@ public class SqlSessionFactoryBeanPostProcessor implements BeanPostProcessor {
             MybatisgxConfiguration mybatisgxConfiguration = (MybatisgxConfiguration) bean;
         }
         return bean;
+    }
+
+    private List<Resource> getDaoResourceList(String[] basePackages) {
+        List<Resource> resourceList = new ArrayList();
+        try {
+            for (String basePackage : basePackages) {
+                basePackage = ResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX + basePackage.replace('.', '/') + "/**.class";
+                ClassUtils.convertClassNameToResourcePath(basePackage).concat("/**.class");
+                Resource[] resources = RESOURCE_PATTERN_RESOLVER.getResources(basePackage);
+                for (Resource resource : resources) {
+                    ClassMetadata classMetadata = METADATA_READER_FACTORY.getMetadataReader(resource).getClassMetadata();
+                    Class<?> clazz = Class.forName(classMetadata.getClassName());
+                    Class<?> daoType = Dao.class;
+                    Repository repository = clazz.getAnnotation(Repository.class);
+                    if (daoType.isAssignableFrom(clazz) && repository != null) {
+                        resourceList.add(resource);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage(), e);
+        } catch (ClassNotFoundException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+        return resourceList;
     }
 }

1af0e536dc5d16595fe6d1f0c80fa724f4314618
cris <xcc137396549@foxmail.com>
2025-12-09 12:15:01 +0800

    [IMP] 优化注解

--- mybatisgx-annotation/src/main/java/com/mybatisgx/annotation/Id.java
+++ mybatisgx-annotation/src/main/java/com/mybatisgx/annotation/Id.java
@@ -1,14 +1,9 @@
 package com.mybatisgx.annotation;
 
-import javax.persistence.GenerationType;
 import java.lang.annotation.*;
-
-import static javax.persistence.GenerationType.AUTO;
 
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface Id {
-
-    GenerationType strategy() default AUTO;
 }

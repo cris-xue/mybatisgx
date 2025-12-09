6df95f131dadb97f52e2d760747c9c55fb7e763a
cris <xcc137396549@foxmail.com>
2025-12-09 12:11:32 +0800

    [IMP] 优化基础api

--- mybatisgx-annotation/src/main/java/com/mybatisgx/annotation/TypeHandler.java
+++ mybatisgx-annotation/src/main/java/com/mybatisgx/annotation/TypeHandler.java
@@ -6,6 +6,11 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+/**
+ * mybatis类型处理器
+ * @author 薛承城
+ * @date 2025/12/9 12:10
+ */
 @Target(FIELD)
 @Retention(RUNTIME)
 public @interface TypeHandler {

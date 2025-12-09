5ec81dab4a54adb7fa9ad9a4fed11fdad2d47376
cris <xcc137396549@foxmail.com>
2025-12-09 11:13:00 +0800

    [ADD] 增加逻辑删除id注解

---
+++ mybatisgx-annotation/src/main/java/com/mybatisgx/annotation/LogicDeleteId.java
@@ -0,0 +1,17 @@
+package com.mybatisgx.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * 逻辑删除id，用于逻辑删除时，标记数据唯一性
+ * @author 薛承城
+ * @date 2025/12/8 11:23
+ */
+@Target({FIELD})
+@Retention(RUNTIME)
+public @interface LogicDeleteId {
+}

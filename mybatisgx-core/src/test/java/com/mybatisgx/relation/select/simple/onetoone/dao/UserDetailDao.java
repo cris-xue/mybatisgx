b7b1a83f146bda38dc51acf628d2fe6aae79a1a7
薛承城 <xcc137396549@foxmail.com>
2026-04-12 23:31:09 +0800

    [ADD] 增加单测场景

--- mybatisgx-core/src/test/java/com/mybatisgx/relation/select/simple/onetoone/dao/UserDetailDao.java
+++ mybatisgx-core/src/test/java/com/mybatisgx/relation/select/simple/onetoone/dao/UserDetailDao.java
@@ -7,4 +7,6 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserDetailDao extends SimpleDao<UserDetail, UserDetail, MultiId> {
+
+    UserDetail findByMultiId(MultiId<Long> multiId);
 }

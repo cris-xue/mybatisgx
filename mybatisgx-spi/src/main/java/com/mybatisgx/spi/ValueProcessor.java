a0f0956d364caa3902ebe25d8194e6b8f946f22a
薛承城 <xcc137396549@foxmail.com>
2026-04-25 10:59:11 +0800

    [ADD] 增加spi模块

---
+++ mybatisgx-spi/src/main/java/com/mybatisgx/spi/ValueProcessor.java
@@ -0,0 +1,25 @@
+package com.mybatisgx.spi;
+
+import com.mybatisgx.api.FieldMeta;
+import com.mybatisgx.api.ValueProcessContext;
+import com.mybatisgx.api.ValueProcessPhase;
+
+import java.util.EnumSet;
+
+public interface ValueProcessor {
+
+    /**
+     * 是否支持当前字段
+     */
+    boolean supports(FieldMeta fieldMeta);
+
+    /**
+     * 支持的生命周期
+     */
+    EnumSet<ValueProcessPhase> phases();
+
+    /**
+     * 执行值处理
+     */
+    Object process(ValueProcessContext context);
+}

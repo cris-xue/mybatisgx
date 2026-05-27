ea060f0e2eafeb58ba35904a4899bd38d60fb420
cris <xcc137396549@foxmail.com>
2026-05-27 14:13:49 +0800

    [REM] 删除不规范的单测

--- mybatisgx-core/src/test/java/com/mybatisgx/relation/select/batch/onetomany/dao/OrgDao.java
+++
@@ -1,9 +0,0 @@
-package com.mybatisgx.relation.select.batch.onetomany.dao;
-
-import com.mybatisgx.dao.SimpleDao;
-import com.mybatisgx.relation.select.batch.onetomany.entity.Org;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface OrgDao extends SimpleDao<Org, Org, Integer> {
-}
--- mybatisgx-core/src/test/java/com/mybatisgx/relation/select/join/manytomany/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.mybatisgx.relation.select.join.manytomany.dao;
-
-import com.mybatisgx.dao.SimpleDao;
-import com.mybatisgx.relation.select.join.manytomany.entity.User;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserDao extends SimpleDao<User, User, Long> {
-}

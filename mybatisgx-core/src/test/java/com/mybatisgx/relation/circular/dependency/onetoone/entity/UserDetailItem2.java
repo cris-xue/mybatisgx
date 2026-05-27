5e3730226c5d2d77007fa5ac6fd2b5af3849d995
cris <xcc137396549@foxmail.com>
2026-05-27 16:40:19 +0800

    [ADD] 增加实体关系循环引用单测

---
+++ mybatisgx-core/src/test/java/com/mybatisgx/relation/circular/dependency/onetoone/entity/UserDetailItem2.java
@@ -0,0 +1,33 @@
+package com.mybatisgx.relation.circular.dependency.onetoone.entity;
+
+import com.mybatisgx.annotation.*;
+import com.mybatisgx.entity.BaseEntity;
+import org.apache.ibatis.mapping.FetchType;
+
+@Entity
+@Table(name = "simple_oto_user_detail_item2_simple")
+public class UserDetailItem2 extends BaseEntity<Long> {
+
+    private String code;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_detail_item1_id")
+    @Fetch(FetchMode.SIMPLE)
+    private UserDetailItem1 userDetailItem1;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public UserDetailItem1 getUserDetailItem1() {
+        return userDetailItem1;
+    }
+
+    public void setUserDetailItem1(UserDetailItem1 userDetailItem1) {
+        this.userDetailItem1 = userDetailItem1;
+    }
+}
---
+++ mybatisgx-core/src/test/java/com/mybatisgx/relation/circular/dependency/onetoone/test/UserDetailDaoTest.java
@@ -0,0 +1,92 @@
+package com.mybatisgx.relation.circular.dependency.onetoone.test;
+
+import com.github.swierkosz.fixture.generator.FixtureGenerator;
+import com.mybatisgx.relation.circular.dependency.onetoone.dao.UserDao;
+import com.mybatisgx.relation.circular.dependency.onetoone.dao.UserDetailDao;
+import com.mybatisgx.relation.circular.dependency.onetoone.dao.UserDetailItem1Dao;
+import com.mybatisgx.relation.circular.dependency.onetoone.dao.UserDetailItem2Dao;
+import com.mybatisgx.relation.circular.dependency.onetoone.entity.User;
+import com.mybatisgx.relation.circular.dependency.onetoone.entity.UserDetail;
+import com.mybatisgx.relation.circular.dependency.onetoone.entity.UserDetailItem1;
+import com.mybatisgx.relation.circular.dependency.onetoone.entity.UserDetailItem2;
+import com.mybatisgx.util.DaoTestUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class UserDetailDaoTest {
+
+    private static int count = 10;
+    private static UserDao userDao;
+    private static UserDetailDao userDetailDao;
+    private static UserDetailItem1Dao userDetailItem1Dao;
+    private static UserDetailItem2Dao userDetailItem2Dao;
+
+    private static List<User> userList = new ArrayList();
+    private static List<UserDetail> userDetailList = new ArrayList();
+    private static List<UserDetailItem1> userDetailItem1List = new ArrayList();
+    private static List<UserDetailItem2> userDetailItem2List = new ArrayList();
+    private static Long firstUserDetailId;
+
+    @BeforeClass
+    public static void beforeClass() {
+        SqlSession sqlSession = DaoTestUtils.getSqlSession(
+                new String[]{"com.mybatisgx.relation.circular.dependency.onetoone.entity"},
+                new String[]{"com.mybatisgx.relation.circular.dependency.onetoone.dao"}
+        );
+        userDao = sqlSession.getMapper(UserDao.class);
+        userDetailDao = sqlSession.getMapper(UserDetailDao.class);
+        userDetailItem1Dao = sqlSession.getMapper(UserDetailItem1Dao.class);
+        userDetailItem2Dao = sqlSession.getMapper(UserDetailItem2Dao.class);
+
+        buildData();
+        userDao.insertBatch(userList, count);
+        userDetailDao.insertBatch(userDetailList, count);
+        userDetailItem1Dao.insertBatch(userDetailItem1List, count);
+        userDetailItem2Dao.insertBatch(userDetailItem2List, count);
+
+        firstUserDetailId = userDetailList.get(0).getId();
+    }
+
+    private static void buildData() {
+        FixtureGenerator fixtureGenerator = new FixtureGenerator();
+        fixtureGenerator.configure().ignoreCyclicReferences();
+
+        for (int i = 0; i < count; i++) {
+            User user = fixtureGenerator.createRandomized(User.class);
+            user.setId(null);
+
+            UserDetail userDetail = user.getUserDetail();
+            userDetail.setId(null);
+            userDetail.setUser(user);
+
+            UserDetailItem1 userDetailItem1 = userDetail.getUserDetailItem1();
+            userDetailItem1.setId(null);
+            userDetailItem1.setUserDetail(userDetail);
+
+            UserDetailItem2 userDetailItem2 = userDetailItem1.getUserDetailItem2();
+            userDetailItem2.setId(null);
+            userDetailItem2.setUserDetailItem1(userDetailItem1);
+
+            userList.add(user);
+            userDetailList.add(userDetail);
+            userDetailItem1List.add(userDetailItem1);
+            userDetailItem2List.add(userDetailItem2);
+        }
+    }
+
+    @Test
+    public void testFindById() {
+        UserDetail dbUserDetail = userDetailDao.findById(firstUserDetailId);
+        Assert.assertNotNull(dbUserDetail);
+        Assert.assertNotNull(dbUserDetail.getUser());
+        Assert.assertNull(dbUserDetail.getUser().getUserDetail());
+    }
+}
